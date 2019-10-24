6f13c78aa3b35b75ddd974a272833cb8f772bb00
Sergey74ua <Sergey74ua@gmail.com>
2019-10-24 23:17:01 +0300

    Update Tetris.java

--- Java/Tetris.java
+++ Java/Tetris.java
@@ -1,26 +1,49 @@
-/*
- * Обучаемся языку Java на примере Тетриса
- * Сергей. Октябрь 2019 года.
- */
- 
-import java.awt.*;
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Timer;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-
-public class Tetris extends JFrame {
-
-  public static void main(String[] args) {
-    Tetris tetris = new Tetris();
-    tetris.window();
-  }
-
-  public void window() {
-    JFrame.setDefaultLookAndFeelDecorated(true);
-    setTitle("Tetris");
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setLocation(200, 100);
-    setSize(800, 600);
-    setResizable(false);
-    setVisible(true);
-  }
+public class Tetris {
+	
+	protected static int block = 40;
+	
+	public static void main(String[] args) {
+		
+		//Окно программы
+		JFrame jFrame = new JFrame("Tetris");
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jFrame.pack();
+        jFrame.setSize(block*10+17, block*20+40);
+		jFrame.setResizable(false);
+		jFrame.setLocationRelativeTo(null);
+		
+		//Отслеживаем нажатия клавиш
+		
+		//Отрисовка в окне
+		int rect = block/5;
+		JPanel panel = new JPanel() {
+            @Override
+            public void paint(Graphics ctx) {
+				setBackground(Color.black);
+                super.paint(ctx);
+				
+				//Тестовые блоки
+				ctx.setColor(Color.green);
+				ctx.drawRoundRect(block*0+1, block*0+1, block-2, block-2, rect, rect);
+				ctx.drawRoundRect(block*1+1, block*1+1, block-2, block-2, rect, rect);
+				ctx.drawRoundRect(block*5+1, block*10+1, block-2, block-2, rect, rect);
+				ctx.drawRoundRect(block*9+1, block*19+1, block-2, block-2, rect, rect);
+				
+				//Сетка
+				ctx.setColor(Color.gray);
+				for (byte i = 0; i <= 10; i++) ctx.drawLine(block*i, 0, block*i, block*20);
+				for (byte i = 0; i <= 20; i++) ctx.drawLine(0, block*i, block*10, block*i);
+            }
+        };
+        jFrame.add(panel);
+		jFrame.setVisible(true);
+    }
 }
+
