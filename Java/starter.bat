e0b06abe2dd18e91249b21cd846e3dfc554afd34
Sergey74ua <Sergey74ua@gmail.com>
2019-10-24 23:16:41 +0300

    Create starter.bat

---
+++ Java/starter.bat
@@ -0,0 +1,13 @@
+@echo off
+chcp 1251 >nul
+title Компиляция и запуск Java-кода
+mode con cols=64 lines=16
+color 0a
+
+echo %TIME% - Compilation
+"C:\Program Files\Java\jdk-13.0.1\bin\javac.exe" Tetris.java
+
+echo %TIME% - Start
+"C:\Program Files\Java\jdk-13.0.1\bin\java.exe" Tetris
+
+pause >nul
