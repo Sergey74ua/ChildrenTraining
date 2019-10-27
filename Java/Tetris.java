60789fe30da9f5f7df80a5ad357e76c93ae08b8f
Sergey74ua <Sergey74ua@gmail.com>
2019-10-27 22:07:39 +0300

    Update Tetris.java

--- Java/Tetris.java
+++ Java/Tetris.java
@@ -13,20 +13,25 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class Tetris {
+public class Tetris extends JPanel {
 	
-	static byte block = 40, t = 0, y = 0;
-	
-	public static void main(String[] args) {
+	//Переменные
+    static int block = 40, x = 0, y = 0;
+
+    public static void main(String[] args) {
 		
-		//Окно программы
-		JFrame jFrame = new JFrame("Tetris");
-		JFrame.setDefaultLookAndFeelDecorated(true);
+		//Окно приложения
+        JFrame jFrame = new JFrame("Tetris");
+		jFrame.setDefaultLookAndFeelDecorated(true);
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jFrame.pack();
 		jFrame.setSize(block*10+17, block*20+40);
 		jFrame.setResizable(false);
 		jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+		
+		//Объект игры
+		Tetris tetris = new Tetris();
+        jFrame.add(tetris);
 		
 		//Отслеживаем нажатия клавиш
 		jFrame.addKeyListener(new KeyAdapter() {
@@ -40,37 +45,36 @@ public class Tetris {
 			}
 		});
 		
-		//Отрисовка в окне
-		while (true) {
-			
-			t++; //для теста
-			JPanel jPanel = new JPanel() {
-				
-				@Override
-				public void paint(Graphics ctx) {
-					setBackground(Color.black);
-					super.paint(ctx);
-					
-					//Сетка
-					ctx.setColor(Color.gray);
-					for (byte i = 0; i <= 10; i++) ctx.drawLine(block*i, 0, block*i, block*20);
-					for (byte i = 0; i <= 20; i++) ctx.drawLine(0, block*i, block*10, block*i);
-					
-					//Тестовые блоки
-					ctx.setColor(Color.green);
-					ctx.fillRect(block*3+1+y*block, block*(-2)+1+t*block, block-1, block-1);
-					ctx.fillRect(block*4+1+y*block, block*(-2)+1+t*block, block-1, block-1);
-					ctx.fillRect(block*5+1+y*block, block*(-2)+1+t*block, block-1, block-1);
-					ctx.fillRect(block*5+1+y*block, block*(-1)+1+t*block, block-1, block-1);
-				}
-			};
-			jFrame.add(jPanel);
-			jFrame.setVisible(true);
-			
-			//Пауза между кадрами
+		//Цикл анимации
+        while (true) {
+            tetris.game();
+            tetris.repaint();
 			try {
 				Thread.sleep(400);
 			} catch (Exception e){}
-		}
-	}
+        }
+    }
+	
+	//Логика игры
+    private void game() {		
+        x++;
+    }
+	
+	//Отрисовка игры
+    public void paint (Graphics ctx) {
+        super.paint(ctx);
+		setBackground(Color.black);
+		
+		//Сетка
+		ctx.setColor(Color.gray);
+		for (int i = 0; i <= 10; i++) ctx.drawLine(block*i, 0, block*i, block*20);
+		for (int i = 0; i <= 20; i++) ctx.drawLine(0, block*i, block*10, block*i);
+		
+		//Фигура
+		ctx.setColor(Color.green);
+		ctx.fillRect(block*3+1+y*block, block*(-2)+1+x*block, block-1, block-1);
+		ctx.fillRect(block*4+1+y*block, block*(-2)+1+x*block, block-1, block-1);
+		ctx.fillRect(block*5+1+y*block, block*(-2)+1+x*block, block-1, block-1);
+		ctx.fillRect(block*5+1+y*block, block*(-1)+1+x*block, block-1, block-1);
+    }
 }
