a2497d383d0191ea915968b2c8ca65ca5637c6a7
Sergey74ua <Sergey74ua@gmail.com>
2019-10-27 22:18:15 +0300

    Update Tetris.java

--- Java/Tetris.java
+++ Java/Tetris.java
@@ -14,12 +14,12 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class Tetris extends JPanel {
-	
-	//Переменные
-    static int block = 40, x = 0, y = 0;
 
-    public static void main(String[] args) {
-		
+	//Переменные
+	static int block = 40, x = 0, y = 0;
+
+	public static void main(String[] args) {
+
 		//Окно приложения
 		JFrame jFrame = new JFrame("Tetris");
 		jFrame.setDefaultLookAndFeelDecorated(true);
@@ -28,11 +28,11 @@ public class Tetris extends JPanel {
 		jFrame.setResizable(false);
 		jFrame.setLocationRelativeTo(null);
 		jFrame.setVisible(true);
-		
+
 		//Объект игры
 		Tetris tetris = new Tetris();
 		jFrame.add(tetris);
-		
+
 		//Отслеживаем нажатия клавиш
 		jFrame.addKeyListener(new KeyAdapter() {
 			public void keyPressed(KeyEvent event) {
@@ -44,37 +44,37 @@ public class Tetris extends JPanel {
 				}
 			}
 		});
-		
+
 		//Цикл анимации
-        while (true) {
-            tetris.game();
-            tetris.repaint();
+		while (true) {
+			tetris.game();
+			tetris.repaint();
 			try {
 				Thread.sleep(400);
 			} catch (Exception e){}
-        }
-    }
-	
+		}
+	}
+
 	//Логика игры
-    private void game() {		
-        x++;
-    }
-	
+	private void game() {
+		x++;
+	}
+
 	//Отрисовка игры
-    public void paint (Graphics ctx) {
-        super.paint(ctx);
+	public void paint (Graphics ctx) {
+		super.paint(ctx);
 		setBackground(Color.black);
-		
+
 		//Сетка
 		ctx.setColor(Color.gray);
 		for (int i = 0; i <= 10; i++) ctx.drawLine(block*i, 0, block*i, block*20);
 		for (int i = 0; i <= 20; i++) ctx.drawLine(0, block*i, block*10, block*i);
-		
+
 		//Фигура
 		ctx.setColor(Color.green);
 		ctx.fillRect(block*3+1+y*block, block*(-2)+1+x*block, block-1, block-1);
 		ctx.fillRect(block*4+1+y*block, block*(-2)+1+x*block, block-1, block-1);
 		ctx.fillRect(block*5+1+y*block, block*(-2)+1+x*block, block-1, block-1);
 		ctx.fillRect(block*5+1+y*block, block*(-1)+1+x*block, block-1, block-1);
-    }
+	}
 }
