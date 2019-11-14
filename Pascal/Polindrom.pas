5077157f1fd39e353870871dd789b30111b4c3cc
Sergey74ua <Sergey74ua@gmail.com>
2019-11-14 22:31:50 +0300

    Create Polindrom.pas

---
+++ Pascal/Polindrom.pas
@@ -0,0 +1,20 @@
+﻿Program Polindrom;
+var a, aZerk, temp : integer;
+label start;
+Begin
+start:
+  write('Введите число: ');
+  readln(a);
+  while a <> aZerk do
+  begin
+    a := a+1;
+    aZerk := 0;
+    temp := a;
+    while temp > 0 do begin
+      aZerk := aZerk*10 + temp mod 10;
+      temp := temp div 10;
+    end;
+  end;
+  writeln('Ближайший больший полиндром: ', aZerk);
+  goto start;
+end.
