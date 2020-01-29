8708939ceffd0d8e9d7c7fa98595025c22d12126
Sergey74ua <Sergey74ua@gmail.com>
2020-01-29 20:24:20 +0300

    Create HelloWorld.cs

---
+++ C#/HelloWorld.cs
@@ -0,0 +1,19 @@
+/*
+ * Уроки по C#
+ * Сергей. Январь 2020 г.
+ */
+
+using System;
+
+namespace HelloWorld
+{
+	class HelloWorld
+	{
+		public static void Main(String[] args)
+		{
+			Console.WriteLine(DateTime.Now);
+			Console.WriteLine("Hello World!");
+			Console.ReadKey();
+		}
+	}
+}
