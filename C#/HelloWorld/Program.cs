6cd6247a0d88eea3413923aaf8d870525aa0a708
Sergey74ua <Sergey74ua@gmail.com>
2020-02-02 19:20:40 +0300

    Create Program.cs

---
+++ C#/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Message;
+
+namespace Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Message message = new Message();
+            message.msg();
+
+            Console.ReadKey();
+        }
+    }
+}
