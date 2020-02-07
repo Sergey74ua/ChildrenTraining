53263dbb1cf275edfac4199e3118b72105e4c81c
Sergey74ua <Sergey74ua@gmail.com>
2020-02-07 08:48:34 +0300

    Update Resources.Designer.cs

--- C#/MyOSU/Properties/Resources.Designer.cs
+++ C#/MyOSU/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace MyOSU.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap maiak {
-            get {
-                object obj = ResourceManager.GetObject("maiak", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
