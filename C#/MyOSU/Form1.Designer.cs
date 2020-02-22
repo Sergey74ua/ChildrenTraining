4f4d68436cdf17cd2fd4edde5dac6a39cd46f5ca
Sergey74ua <Sergey74ua@gmail.com>
2020-02-22 23:15:45 +0300

    Update Form1.Designer.cs

--- C#/MyOSU/Form1.Designer.cs
+++ C#/MyOSU/Form1.Designer.cs
@@ -31,10 +31,11 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.txtScore = new System.Windows.Forms.Label();
+            this.txtTotalScore = new System.Windows.Forms.Label();
             this.txtLevel = new System.Windows.Forms.Label();
             this.txtTimer = new System.Windows.Forms.Label();
             this.txtPixel = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // timer1
@@ -43,19 +44,19 @@
             this.timer1.Interval = 16;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // txtScore
+            // txtTotalScore
             // 
-            this.txtScore.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtScore.AutoSize = true;
-            this.txtScore.BackColor = System.Drawing.Color.Transparent;
-            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.txtScore.Location = new System.Drawing.Point(980, 20);
-            this.txtScore.Name = "txtScore";
-            this.txtScore.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.txtScore.Size = new System.Drawing.Size(163, 55);
-            this.txtScore.TabIndex = 0;
-            this.txtScore.Text = "Score:";
-            this.txtScore.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.txtTotalScore.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTotalScore.AutoSize = true;
+            this.txtTotalScore.BackColor = System.Drawing.Color.Transparent;
+            this.txtTotalScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtTotalScore.Location = new System.Drawing.Point(980, 20);
+            this.txtTotalScore.Name = "txtTotalScore";
+            this.txtTotalScore.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtTotalScore.Size = new System.Drawing.Size(163, 55);
+            this.txtTotalScore.TabIndex = 0;
+            this.txtTotalScore.Text = "Score:";
+            this.txtTotalScore.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // txtLevel
             // 
@@ -92,6 +93,18 @@
             this.txtPixel.TabIndex = 3;
             this.txtPixel.Text = "pixel";
             // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.BackColor = System.Drawing.Color.Transparent;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtScore.ForeColor = System.Drawing.Color.Red;
+            this.txtScore.Location = new System.Drawing.Point(987, 75);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(15, 16);
+            this.txtScore.TabIndex = 4;
+            this.txtScore.Text = "0";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -100,10 +113,11 @@
             this.BackgroundImage = global::MyOSU.Resource1.fon;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1184, 761);
+            this.Controls.Add(this.txtScore);
             this.Controls.Add(this.txtPixel);
             this.Controls.Add(this.txtLevel);
             this.Controls.Add(this.txtTimer);
-            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.txtTotalScore);
             this.Cursor = System.Windows.Forms.Cursors.Cross;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -119,10 +133,11 @@
 
         #endregion
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label txtTotalScore;
         private System.Windows.Forms.Label txtLevel;
         private System.Windows.Forms.Label txtTimer;
         private System.Windows.Forms.Label txtPixel;
+        private System.Windows.Forms.Label txtScore;
     }
 }
 
