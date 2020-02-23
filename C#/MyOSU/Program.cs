64e549d97f73a7f4f3716a447c737b973ee14fa4
Sergey74ua <Sergey74ua@gmail.com>
2020-02-23 14:49:21 +0300

    Update Program.cs

--- C#/MyOSU/Program.cs
+++ C#/MyOSU/Program.cs
@@ -5,15 +5,12 @@ namespace MyOSU
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormMyOsu());
         }
     }
 }
