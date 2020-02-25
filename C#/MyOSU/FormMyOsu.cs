bcd4eae2674b4571a79c820f3795ea8eea99ebca
Sergey74ua <Sergey74ua@gmail.com>
2020-02-25 17:05:04 +0300

    Update FormMyOsu.cs

--- C#/MyOSU/FormMyOsu.cs
+++ C#/MyOSU/FormMyOsu.cs
@@ -11,13 +11,14 @@ namespace MyOSU
     {
         //Объекты и переменные
         private Bitmap aim = Resource1.aim;
-        private Point target = Point.Empty;
-        private Random random = new Random();
-        private Stopwatch stopwatch = new Stopwatch();
         private Pen pen = new Pen(Color.Black, 3);
+        private Point cursor, target = Point.Empty;
+        private Rectangle aimPosition, targetPosition;
         private SoundPlayer soundPlayer = new SoundPlayer(Resource1.click);
+        private Stopwatch stopwatch = new Stopwatch();
+        private Random random = new Random();
         private double gipotenuza;
-        private int step, time, score, totalScore;
+        private int katetX, katetY, step, time, score, totalScore;
 
         //Запуск окна
         public FormMyOsu()
@@ -39,16 +40,48 @@ namespace MyOSU
             g.SmoothingMode = SmoothingMode.AntiAlias;
 
             //Отрисовка прицела и цели
-            Point cursor = PointToClient(Cursor.Position);
-            Rectangle aimPosition = new Rectangle(cursor.X - 50, cursor.Y - 50, 100, 100);
-            Rectangle targetPosition = new Rectangle(target.X - 50, target.Y - 50, 100, 100);
+            cursor = PointToClient(Cursor.Position);
+            aimPosition = new Rectangle(cursor.X - 50, cursor.Y - 50, 100, 100);
+            targetPosition = new Rectangle(target.X - 50, target.Y - 50, 100, 100);
             g.DrawImage(aim, aimPosition);
             g.DrawEllipse(pen, targetPosition);
+        }
 
-            //Считаем точность попадания
-            int katetX = aimPosition.X - targetPosition.X,
-                katetY = aimPosition.Y - targetPosition.Y;
-            gipotenuza = (int) Math.Sqrt(katetX * katetX + katetY * katetY);
+        //Перемещение цели (рекурсия)
+        private void randomTarget()
+        {
+            Point _target = target;
+            target.X = Width / 2 + random.Next(-4, 4) * 100;
+            target.Y = Height / 2 + random.Next(-3, 3) * 100;
+            if (target == _target) randomTarget();
+        }
+
+        //Ход игры
+        private void StepGame()
+        {
+            //Счетчик ходов и таймер
+            step++;
+            time = (int) stopwatch.Elapsed.TotalMilliseconds;
+            soundPlayer.Play();
+
+            //Точность попадания
+            katetX = aimPosition.X - targetPosition.X;
+            katetY = aimPosition.Y - targetPosition.Y;
+            gipotenuza = (int)Math.Sqrt(katetX * katetX + katetY * katetY);
+
+            //Информационная панель
+            score = (int) (1 / gipotenuza * time);
+            totalScore += score;
+            txtTotalScore.Text = ("Score:\n" + totalScore.ToString());
+            txtScore.Text = (score.ToString());
+            txtLevel.Text = ("step:  " + step.ToString());
+            txtTimer.Text = ("timer: " + time.ToString());
+            txtPixel.Text = ("pixel:  " + gipotenuza.ToString());
+
+            //Рестарт хода
+            randomTarget();
+            Refresh();
+            stopwatch.Restart();
         }
 
         //Анимация графики
@@ -67,37 +100,6 @@ namespace MyOSU
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             StepGame();
-        }
-
-        //Ход игры
-        private void StepGame()
-        {
-            //Счетчик ходов и таймер
-            step++;
-            time = (int) stopwatch.Elapsed.TotalMilliseconds;
-            soundPlayer.Play();
-
-            //Информационная панель
-            score = (int) (1 / gipotenuza * time);
-            totalScore += score;
-            txtTotalScore.Text = ("Score:\n" + totalScore.ToString());
-            txtScore.Text = (score.ToString());
-            txtLevel.Text = ("step:  " + step.ToString());
-            txtTimer.Text = ("timer: " + time.ToString());
-            txtPixel.Text = ("pixel:  " + gipotenuza.ToString());
-
-            randomTarget();
-            Refresh();
-            stopwatch.Restart();
-        }
-
-        //Перемещение цели (реурсия)
-        private void randomTarget()
-        {
-            Point _target = target;
-            target.X = Width/2 + random.Next(-4, 4) * 100;
-            target.Y = Height/2 + random.Next(-3, 3) * 100;
-            if (target == _target) randomTarget();
         }
     }
 }
