4061f686fc720f30a713dda9ad288e3dd45d8536
Sergey74ua <Sergey74ua@gmail.com>
2020-03-08 10:39:19 +0300

    Create Program.cs

---
+++ C#/Password/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Переменные
+            string password, _password;
+
+            //Пароль для входа
+            password = "123456";
+
+            //Проверка пароля
+            _password = "0";
+            while (password != _password)
+            {
+                Console.Write("Введите пароль: ");
+                _password = Console.ReadLine();
+                if (_password != password) Console.WriteLine("в доступе отказано\n");
+            }
+
+            //Открываем доступ
+            for (byte i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i + " ДОСТУП ОТКРЫТ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
