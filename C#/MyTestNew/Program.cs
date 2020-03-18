4efaa3d8c25dda8cbd85d01e84e28364bb43d2aa
Sergey74ua <Sergey74ua@gmail.com>
2020-03-18 15:17:51 +0300

    Create Program.cs

---
+++ C#/MyTestNew/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MyTestNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            unsafe
+            {
+                //int variable; //Вариант 1 - переменная вне цикла
+
+                stopwatch.Start();
+                for (int i = 0; i < 1000; i++)
+                {
+                    int variable; //Вариант 2 - переменная в цикле
+
+                    int* pointer = &variable; //указатель
+                    ulong address = (ulong)pointer; //адрес
+                    Console.Write("Адрес переменной: {0}", address);
+                    Console.WriteLine(" и ее значение: {0}", variable++);
+                }
+                stopwatch.Stop();
+                Console.WriteLine(stopwatch.ElapsedMilliseconds);
+            }
+            Console.ReadLine();
+        }
+    }
+}
