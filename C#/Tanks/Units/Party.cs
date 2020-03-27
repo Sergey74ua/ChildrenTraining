d11dff1aa36c2e9a00a060a3b5b3487cad1b051e
Sergey74ua <Sergey74ua@gmail.com>
2020-03-27 09:22:51 +0300

    Create Party.cs

---
+++ C#/Tanks/Units/Party.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Tanks
+{
+    class Party
+    {
+        //Список танков
+        private const byte count = 6;
+        private List<Tank> PartyTank = new List<Tank>(count);
+
+        public void DrawPartyTank(Graphics g)
+        {
+            //Заполняем список танками
+            while (PartyTank.Count < count)
+            {
+                PartyTank.Add(new Tank()
+                {
+                    party = Color.DarkRed
+                });
+            }
+
+            byte x = 0; //*************************************************************************
+            //Отрисовываем танки по списку
+            foreach (Tank tank in PartyTank)
+            {
+                x++; //****************************************************************************
+                if (x % 2 == 0)
+                    tank.party = Color.DarkBlue; //************************************************
+                    
+                tank.DrawTank(g, x);
+            }
+        }
+    }
+}
