e9ed95ee5dd62f3a197ec0cac16b415dca29159e
Sergey74ua <Sergey74ua@gmail.com>
2020-03-27 09:21:57 +0300

    Create FormTanks.cs

---
+++ C#/Tanks/FormTanks.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Tanks
+{
+    public partial class Tanks : Form
+    {
+        Party party;
+        Graphics g;
+
+        public Tanks()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.OptimizedDoubleBuffer |
+                    ControlStyles.AllPaintingInWmPaint   |
+                    ControlStyles.UserPaint, true);
+            UpdateStyles();
+        }
+
+        //Загрузка окна
+        private void Tanks_Load(object sender, EventArgs e)
+        {
+            party = new Party();
+        }
+
+        //Отрисовка кадра
+        private void Tanks_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            party.DrawPartyTank(g);
+        }
+
+        //Таймер обновления кадра
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+    }
+}
