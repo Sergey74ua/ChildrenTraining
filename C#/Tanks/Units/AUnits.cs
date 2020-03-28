bd118a07b03204c6e30bc887da3ae51e426e05ef
Sergey74ua <Sergey74ua@gmail.com>
2020-03-28 19:49:03 +0300

    Update AUnits.cs

--- C#/Tanks/Units/AUnits.cs
+++ C#/Tanks/Units/AUnits.cs
@@ -5,6 +5,7 @@ namespace Tanks
     abstract class AUnits
     {
         public Point position;  //позиция
+        public Point target;    //цель
         public float vector;    //вектор
     }
 }
