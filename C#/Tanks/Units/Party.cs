d3a43234499811fcbdbf68a32e520bcb2d02aa6a
Sergey74ua <Sergey74ua@gmail.com>
2020-03-28 19:49:06 +0300

    Update Party.cs

--- C#/Tanks/Units/Party.cs
+++ C#/Tanks/Units/Party.cs
@@ -1,35 +1,49 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 
 namespace Tanks
 {
     class Party
     {
-        //Список танков
-        private const byte count = 6;
-        private List<Tank> PartyTank = new List<Tank>(count);
+        public byte count = 20;
+        private List<Tank> PartyTank = new List<Tank>();
+        private Random random = new Random();
 
-        public void DrawPartyTank(Graphics g)
+        //Заполняем список танками
+        public List<Tank> CreatePartyTank(byte count, Color party)
         {
-            //Заполняем список танками
             while (PartyTank.Count < count)
             {
                 PartyTank.Add(new Tank()
                 {
-                    party = Color.DarkRed
+                    party = party,
+                    position = PointRandom(party),
+                    target = new Point(640, 720)
                 });
             }
+            return PartyTank;
+        }
 
-            byte x = 0; //*************************************************************************
-            //Отрисовываем танки по списку
+        //Отрисовываем танки по списку
+        public void DrawPartyTank(Graphics g)
+        {
             foreach (Tank tank in PartyTank)
-            {
-                x++; //****************************************************************************
-                if (x % 2 == 0)
-                    tank.party = Color.DarkBlue; //************************************************
-                    
-                tank.DrawTank(g, x);
+            {                    
+                tank.DrawTank(g, tank.party);
             }
+        }
+
+        //Случайная позиция
+        private Point PointRandom(Color party)
+        {
+            Point point = Point.Empty;
+            if (party == Color.DarkBlue)
+                point.X = random.Next(50, 320);
+            if (party == Color.DarkRed)
+                point.X = random.Next(960, 1230);
+            point.Y = random.Next(50, 670);
+            return point;
         }
     }
 }
