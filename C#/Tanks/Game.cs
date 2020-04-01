edc8ddb4b6264b0f2885516ac03ae971d3d7cf44
Sergey74ua <Sergey74ua@gmail.com>
2020-04-01 19:52:02 +0300

    Update Game.cs

--- C#/Tanks/Game.cs
+++ C#/Tanks/Game.cs
@@ -16,17 +16,13 @@ namespace Tanks
             if (party == Color.DarkRed)
                 point.X = random.Next(740, 1230);
             point.Y = random.Next(50, 670);
+
             return point;
         }
 
         //Перемещение танков
         public Tank Step(Tank tank, Point cursor)
         {
-            //tank.position.X += random.Next(-2, 3);
-            //tank.position.Y += random.Next(-1, 2);
-            //tank.target.X = (int) x;
-            //tank.target.Y = (int) x *3/5;
-
             tank.target.X = cursor.X;
             tank.target.Y = cursor.Y;
 
