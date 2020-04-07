16356d2c1ff27557cac4eb5d5170a78497e54314
Sergey74ua <Sergey74ua@gmail.com>
2020-04-08 00:39:38 +0300

    Create Shot.cs

---
+++ C#/Tanks/Units/Shot.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+
+namespace Tanks
+{
+    class Shot : AObject
+    {
+        private Pen pen;
+        private PointF position0;
+        protected float angle, catetX, catetY;
+
+        public void DrawShot(Graphics g, Color color)
+        {
+            position = Position();
+            position0.X = position.X + 16;
+            position0.Y = position.Y + 10;
+            target = new Point(1000, 600);
+            pen = new Pen(color, 3);
+
+            g.TranslateTransform(position.X, position.Y);
+            g.DrawLine(pen, position0, position);
+            g.ResetTransform();
+        }
+
+        //Полет снаряда
+        public PointF Position()
+        {
+            speed = 8;
+
+            //Определяем угол на цель
+            catetX = target.X - position.X;
+            catetY = target.Y - position.Y;
+            angle = (float)(Math.Atan2(catetY, catetX) * 180 / Math.PI + 90);
+            if (angle < 0) angle += 360;
+
+            position.X += speed * (float)Math.Cos(angle);
+            position.Y += speed * (float)Math.Sin(angle);
+
+            return position;
+        }
+    }
+}
