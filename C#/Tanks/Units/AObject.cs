7fb0a361088f1df8ee390b65fcbef238d1b1e67e
Sergey74ua <Sergey74ua@gmail.com>
2020-04-08 00:39:35 +0300

    Create AObject.cs

---
+++ C#/Tanks/Units/AObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace Tanks
+{
+    abstract class AObject
+    {
+        public Color party;     //команда
+        public PointF position; //позиция
+        public PointF target;   //цель
+        public float vector;    //вектор
+        public float speed;     //скорость
+    }
+}
