2da52807d8eb699948125880a586ebdf862f0227
Sergey74ua <Sergey74ua@gmail.com>
2020-04-18 22:17:42 +0300

    Create Actions.cs

---
+++ C#/Tanks/Units/Actions.cs
@@ -0,0 +1,41 @@
+﻿namespace Tanks
+{
+    public enum A : byte //********  В О Т   О Н О   Н А Д О  ?  ********
+    {
+        WAIT,       //ожидание
+        LOOC,       //осмотр    ********
+        FIND,       //разветка
+        ROTATE,     //поворот   ********
+        RUN,        //движение  ********
+        LOAD,       //зарядка   ********
+        FIRE,       //выстрел
+        BACK,       //отъезд
+        REPAIR,     //ремонт    ********
+        LEAVE       //смерть
+    }
+
+    class Actions
+    {
+        //Выбор действия
+        public A ChoiseActions(dynamic unit) //******** Д О В Е С Т И   Д О   У М А ********
+        {
+            //Танк уничтожен
+            if (unit.life <= 0)
+                return A.LEAVE;
+
+            //Отсупление
+            if (unit.life < 5)
+                return A.BACK;
+
+            //Атака цели
+            if (unit.target != null)
+                return A.FIRE;
+
+            //Поиск цели
+            if (unit.target == null)
+                return A.FIND;
+
+            return A.WAIT; //******** У Б Р А Т Ь   В   К О Н Е Ц   Ф У Н К И Й ********
+        }
+    }
+}
