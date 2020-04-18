eac06181af78fd427e91fd07ac40708597a8df9b
Sergey74ua <Sergey74ua@gmail.com>
2020-04-18 22:17:28 +0300

    Update Game.cs

--- C#/Tanks/Game.cs
+++ C#/Tanks/Game.cs
@@ -5,22 +5,26 @@ namespace Tanks
     class Game
     {
         private Party PartyRed, PartyBlue;
-        private Shot shot = new Shot(); //************************ В Р Е М Е Н Н О ************************
+        private Shots ListShot;
 
         public void StartGame()
         {
             PartyRed = new Party();
             PartyBlue = new Party();
+            ListShot = new Shots();
+
             PartyRed.CreateListUnits(PartyRed.count, Color.DarkRed);
             PartyBlue.CreateListUnits(PartyBlue.count, Color.DarkBlue);
+            //******** отсюда надо как-то вызвать функцию создания выстрелов ... ********
+            ListShot.NewShot(new PointF(10, 10), new PointF(1000, 600), Color.DarkOrange); //******** В Р Е М Е Н Н О ********
+            ListShot.NewShot(new PointF(20, 600), new PointF(1100, 30), Color.LimeGreen); //******** В Р Е М Е Н Н О ********
         }
 
         public void StepGame(Graphics g, Point cursor)
         {
             PartyRed.DrawListUnits(g, cursor);
             PartyBlue.DrawListUnits(g, cursor);
-            
-            shot.DrawShot(g, Color.DarkOrange); //**************** В Р Е М Е Н Н О ****************
+            ListShot.DrawListShot(g);
         }
     }
 }
