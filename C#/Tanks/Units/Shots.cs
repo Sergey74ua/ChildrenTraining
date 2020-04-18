226239aec9d5696d0224b43f199f3d4538cbd088
Sergey74ua <Sergey74ua@gmail.com>
2020-04-18 22:18:05 +0300

    Create Shots.cs

---
+++ C#/Tanks/Units/Shots.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Tanks
+{
+    class Shots
+    {
+        private List<Shot> ListShot = new List<Shot>();
+
+        //Добавляем выстрел
+        public void NewShot(PointF position, PointF target, Color party)
+        {
+            ListShot.Add(new Shot
+            {
+                party = party,
+                position = position,
+                target = target
+            });
+        }
+
+        //Отрисовываем выстрелы по списку
+        public void DrawListShot(Graphics g)
+        {
+            foreach (dynamic shot in ListShot)
+            {
+                shot.DrawShot(g);
+            }
+        }
+    }
+}
