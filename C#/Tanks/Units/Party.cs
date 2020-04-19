88f3bc923c8aa7483ca8e2e2a9cf4147c17f2496
Sergey74ua <Sergey74ua@gmail.com>
2020-04-19 23:22:22 +0300

    Update Party.cs

--- C#/Tanks/Units/Party.cs
+++ C#/Tanks/Units/Party.cs
@@ -4,33 +4,42 @@ using System.Drawing;
 
 namespace Tanks
 {
-    class Party
+    sealed class Party
     {
         public byte count = 5;
 
         private List<object> ListUnits = new List<object>();
         private Actions action = new Actions();
-        private Random random = new Random();
+        private readonly Random random = new Random();
 
         //Заполняем список юнитами
         public List<object> CreateListUnits(byte count, Color party)
         {
+            PointF position;    //случайная позиция
+            float vector;       //поворот на центр
+
             for (byte i = 0; i < count; i++)
             {
+                position = Start(party);
+                vector = Vector(position);
                 ListUnits.Add(new Tank
                 {
-                    id = i + 1,
                     party = party,
-                    position = Start(party),
+                    position = position,
+                    vectorBody = vector,
+                    vectorTower = vector,
                     speed = 0.5f,
                     life = 40
                 });
 
+                position = Start(party);
+                vector = Vector(position);
                 ListUnits.Add(new Car
                 {
-                    id = i + 11,
                     party = party,
-                    position = Start(party),
+                    position = position,
+                    vectorBody = vector,
+                    vectorTower = vector,
                     speed = 1.0f,
                     life = 10
                 });
@@ -39,28 +48,37 @@ namespace Tanks
             return ListUnits;
         }
 
-        //Случайная позиция
-        private Point Start(Color party)
-        {
-            Point point = Point.Empty;
-            if (party == Color.DarkBlue)
-                point.X = random.Next(50, 590);
-            if (party == Color.DarkRed)
-                point.X = random.Next(690, 1230);
-            point.Y = random.Next(50, 670);
-
-            return point;
-        }
-
         //Отрисовываем юнитов по списку
         public void DrawListUnits(Graphics g, Point cursor)
         {
             foreach (dynamic unit in ListUnits)
             {
                 unit.target = cursor; //должна быть ссылка на функцию определения таргета
-                unit.action = action.ChoiseActions(unit);
+                action.SwitchAct(unit); //******** Д О Д Е Л А Т Ь ********
                 unit.DrawUnit(g, unit.party);
             }
+        }
+
+        //Насальная случайная позиция
+        private PointF Start(Color party)
+        {
+            PointF point = PointF.Empty;
+            if (party == Color.DarkBlue)
+                point.X = random.Next(50, 440);
+            if (party == Color.DarkRed)
+                point.X = random.Next(840, 1230);
+            point.Y = random.Next(50, 670);
+
+            return point;
+        }
+
+        //Начальный угол на центр
+        private float Vector(PointF position)
+        {
+            float vector = (float)(Math.Atan2(360 - position.Y, 640 - position.X) * 180 / Math.PI + 90);
+            if (vector < 0) vector += 360;
+
+            return vector;
         }
     }
 }
