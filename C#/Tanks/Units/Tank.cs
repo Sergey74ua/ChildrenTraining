169fb31c273da6fdc7794846d99988a2845e9527
Sergey74ua <Sergey74ua@gmail.com>
2020-04-19 23:22:29 +0300

    Update Tank.cs

--- C#/Tanks/Units/Tank.cs
+++ C#/Tanks/Units/Tank.cs
@@ -2,42 +2,38 @@
 
 namespace Tanks
 {
-    sealed class Tank : AUnits
+    class Tank : AUnits, IDrawn
     {
         //Изображение танка
-        private Bitmap bitmap = new Bitmap(Properties.Resources.tank4);
-        private Rectangle body = new Rectangle(new Point(0, 0), new Size(128, 128));
-        private Rectangle tower = new Rectangle(new Point(128, 0), new Size(128, 128));
-        private Rectangle bodyShadow = new Rectangle(new Point(0, 128), new Size(128, 128));
-        private Rectangle towerShadow = new Rectangle(new Point(128, 128), new Size(128, 128));
+        private readonly Bitmap bitmap = new Bitmap(Properties.Resources.tank4);
+        private readonly Rectangle body = new Rectangle(new Point(0, 0), new Size(128, 128));
+        private readonly Rectangle tower = new Rectangle(new Point(128, 0), new Size(128, 128));
+        private readonly Rectangle bodyShadow = new Rectangle(new Point(0, 128), new Size(128, 128));
+        private readonly Rectangle towerShadow = new Rectangle(new Point(128, 128), new Size(128, 128));
+        private readonly byte shadow = Tanks.shadow;
         private SolidBrush solidBrush;
-        private byte shadow = Tanks.shadow;
 
         //Отрисовка танка
         public void DrawUnit(Graphics g, Color party)
         {
-            action = A.WAIT;
-            vector = Vector(vector, 1.0f);
-            vectorTower = Vector(vectorTower, 2.0f);
-            position = Position(speed);
             solidBrush = new SolidBrush(party);
 
             #region ** Этапы отрисовки танка **
             //Тень корпуса
             g.TranslateTransform(position.X + shadow, position.Y + shadow);
-            g.RotateTransform(vector);
+            g.RotateTransform(vectorBody);
             g.DrawImage(bitmap, -64, -64, bodyShadow, GraphicsUnit.Pixel);
             g.ResetTransform();
 
             //Цвет команды
             g.TranslateTransform(position.X, position.Y);
-            g.RotateTransform(vector);
+            g.RotateTransform(vectorBody);
             g.FillRectangle(solidBrush, -26, -52, 52, 100);
             g.ResetTransform();
 
             //Корпус
             g.TranslateTransform(position.X, position.Y);
-            g.RotateTransform(vector);
+            g.RotateTransform(vectorBody);
             g.DrawImage(bitmap, -64, -64, body, GraphicsUnit.Pixel);
             g.ResetTransform();
 
