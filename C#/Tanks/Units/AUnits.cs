b1fb7c21472703170e248564daa09b406bd36760
Sergey74ua <Sergey74ua@gmail.com>
2020-04-19 23:22:12 +0300

    Update AUnits.cs

--- C#/Tanks/Units/AUnits.cs
+++ C#/Tanks/Units/AUnits.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Drawing;
+﻿using System.Drawing;
 
 namespace Tanks
 {
     abstract class AUnits : AObject
     {
+        private static uint ID;
+
         //Общие поля юнитов
-        public int id;                  //имя
-        public A action;                //действие
+        public uint id = ++ID;          //имя
+        public Act action = Act.WAIT;   //действие
         public float life;              //жизнь
-        protected float vector;         //вектор
-        protected float vectorTower;    //вектор башни
-        private float angle, catetX, catetY;  //*** П Е Р Е Д Е Л А Т Ь ***
+        public float vectorBody;        //вектор корпуса
+        public float vectorTower;       //вектор башни
 
         //Данные для отисовки
-        private SolidBrush solidBrushFont = new SolidBrush(Color.LightGreen);
-        private Font font = new Font("Arial", 10, FontStyle.Bold, GraphicsUnit.Point);
-        private Pen pen = new Pen(Color.Green, 2);
+        private readonly SolidBrush solidBrushFont = new SolidBrush(Color.LightGreen);
+        private readonly Font font = new Font("Arial", 10, FontStyle.Bold, GraphicsUnit.Point);
+        private readonly Pen pen = new Pen(Color.Green, 2);
 
         //Отрисовка имени и жизни
-        public void DrawInfo(Graphics g)
+        protected void DrawInfo(Graphics g)
         {
             //Наименование
             g.TranslateTransform(position.X, position.Y);
@@ -30,44 +30,6 @@ namespace Tanks
             g.TranslateTransform(position.X, position.Y);
             g.DrawLine(pen, -32, -26, 32, -26);
             g.ResetTransform();
-        }
-
-        //Направление юнита
-        public float Vector(float vector, float speed)
-        {
-            //Определяем угол на цель
-            catetX = target.X - position.X;
-            catetY = target.Y - position.Y;
-            angle = (float)(Math.Atan2(catetY, catetX) * 180 / Math.PI + 90);
-            if (angle < 0) angle += 360;
-
-            //Текущий угол поворота к цели
-            if (Math.Abs(vector - angle) > speed)
-            {
-                if ((vector < angle && (angle - vector) < 180) ^ (angle - vector) > -180)
-                    vector = (vector - speed + 360) % 360;
-                else
-                    vector = (vector + speed) % 360;
-            }
-            else
-            {
-                vector = angle;
-            }
-
-            return vector;
-        }
-
-        //Перемещение юнита
-        public PointF Position(float speed)
-        {
-            double gipotenuza = Math.Sqrt(catetX * catetX + catetY * catetY);
-            if (gipotenuza > 128 && vector == angle)
-            {
-                position.X += speed * (float)Math.Cos(vector);
-                position.Y += speed * (float)Math.Sin(vector);
-            }
-            
-            return position;
         }
     }
 }
