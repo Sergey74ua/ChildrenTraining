eb6a1b6e25ba1a91bdb04f71d61b934718a3267f
Sergey74ua <Sergey74ua@gmail.com>
2020-04-19 23:22:27 +0300

    Update Shots.cs

--- C#/Tanks/Units/Shots.cs
+++ C#/Tanks/Units/Shots.cs
@@ -1,21 +1,31 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 
 namespace Tanks
 {
     class Shots
     {
-        private List<Shot> ListShot = new List<Shot>();
+        public List<Shot> ListShot = new List<Shot>();
 
-        //Добавляем выстрел
+        //Добавляем выстрел + залп
         public void NewShot(PointF position, PointF target, Color party)
         {
+            // ... прописать залп
             ListShot.Add(new Shot
             {
                 party = party,
                 position = position,
                 target = target
             });
+        }
+
+        //Удаляем выстрел + взрыв        
+        public void RemoveShot(Shot shot) //******** проба ********
+        {
+            if (Math.Abs(shot.position.X - shot.target.X) < 100 && Math.Abs(shot.position.Y - shot.target.Y) < 100) 
+                ListShot.Remove(shot);
+            // ... прописать взрыв
         }
 
         //Отрисовываем выстрелы по списку
