4efef7e2b8943e2e27d8db4369b209b47326e3e6
Sergey74ua <Sergey74ua@gmail.com>
2020-04-25 23:41:57 +0300

    Update AUnits.cs

--- C#/Tanks/Units/AUnits.cs
+++ C#/Tanks/Units/AUnits.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 
 namespace Tanks
 {
@@ -8,11 +9,11 @@ namespace Tanks
 
         //Общие поля юнитов
         public uint id = ++ID;          //имя
-        public Act action = Act.WAIT;   //действие
         public float life;              //жизнь
-        public float vectorBody;        //вектор корпуса
         public float vectorTower;       //вектор башни
-        public uint timeShot = 0; //***************************** В Р Е М Е Н Н О *****************************
+        public uint timeShot;          //******** перезарядка (проба)
+        private float angle, catetX, catetY;  //******** П Е Р Е Д Е Л А Т Ь ********
+        private double gipotenuza;
 
         //Данные для отисовки
         private readonly SolidBrush solidBrushFont = new SolidBrush(Color.LightGreen);
@@ -27,6 +28,43 @@ namespace Tanks
             g.DrawString("= " + id.ToString() + " =", font, solidBrushFont, -20, -42);
             g.DrawLine(pen, -32, -26, 32, -26);
             g.ResetTransform();
+        }
+
+        //Направление юнита
+        public float Vector(float vector, float speed)
+        {
+            //Определяем угол на цель
+            catetX = target.X - position.X;
+            catetY = target.Y - position.Y;
+            gipotenuza = Math.Sqrt(catetX * catetX + catetY * catetY);
+            angle = (float)(Math.Atan2(catetY, catetX)*180/Math.PI+90);
+            if (angle < 0) angle += 360;
+
+            //Текущий угол поворота к цели
+            if (Math.Abs(vector - angle) > speed)
+            {
+                if ((vector < angle && (angle - vector) < 180) ^ (angle - vector) > -180)
+                    vector = (vector - speed+360)%360;
+                else
+                    vector = (vector + speed)%360;
+            }
+            else
+                vector = angle;
+
+            return vector;
+        }
+
+        //Перемещение юнита
+        public PointF Position(PointF position, float speed)
+        {
+            gipotenuza = Math.Sqrt(catetX*catetX + catetY*catetY);
+            if (gipotenuza > 128 && vector == angle) //******** вынести проверку в логику ********
+            {
+                position.X += speed*(float)Math.Cos(vector);
+                position.Y += speed*(float)Math.Sin(vector);
+            }
+
+            return position;
         }
     }
 }
