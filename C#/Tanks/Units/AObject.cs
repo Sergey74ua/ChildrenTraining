b87221b4a274bbd5ea3397f84b6304118bd5246f
Sergey74ua <Sergey74ua@gmail.com>
2020-04-25 23:41:54 +0300

    Update AObject.cs

--- C#/Tanks/Units/AObject.cs
+++ C#/Tanks/Units/AObject.cs
@@ -7,6 +7,7 @@ namespace Tanks
         public Color party;     //команда
         public PointF position; //позиция
         public PointF target;   //цель
+        public float vector;    //вектор
         public float speed;     //скорость
     }
 }
