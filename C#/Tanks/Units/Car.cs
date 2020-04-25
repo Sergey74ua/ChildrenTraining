d66409574d7ed62e218262fcf2eac9ca879df698
Sergey74ua <Sergey74ua@gmail.com>
2020-04-25 23:41:59 +0300

    Update Car.cs

--- C#/Tanks/Units/Car.cs
+++ C#/Tanks/Units/Car.cs
@@ -12,38 +12,41 @@ namespace Tanks
         private readonly Rectangle tower = new Rectangle(new Point(64, 0), size);
         private readonly Rectangle bodyShadow = new Rectangle(new Point(0, 64), size);
         private readonly Rectangle towerShadow = new Rectangle(new Point(64, 64), size);
-        private readonly byte shadow = Tanks.shadow / 2;
+        private readonly byte shadow = Tanks.shadow/2;
         private SolidBrush solidBrush;
 
         //Снаряд танка ******** заменить на пулеметную очередь ********
-        public Shot shot = new Shot();
+        public Shot shot;
 
         //Отрисовка танка
         public void DrawUnit(Graphics g, Color party)
         {
             solidBrush = new SolidBrush(party);
+            position = Position(position, speed);
+            vector = Vector(vector, speed);
+            vectorTower = Vector(vectorTower, speed*2);
 
             #region ** Этапы отрисовки машины **
             //Тень корпуса
             g.TranslateTransform(position.X + shadow, position.Y + shadow);
-            g.RotateTransform(vectorBody);
+            g.RotateTransform(vector);
             g.DrawImage(bitmap, -32, -26, bodyShadow, GraphicsUnit.Pixel);
             g.ResetTransform();
 
             //Цвет команды
             g.TranslateTransform(position.X, position.Y);
-            g.RotateTransform(vectorBody);
+            g.RotateTransform(vector);
             g.FillRectangle(solidBrush, -12, -18, 23, 50);
             g.ResetTransform();
 
             //Корпус
             g.TranslateTransform(position.X, position.Y);
-            g.RotateTransform(vectorBody);
+            g.RotateTransform(vector);
             g.DrawImage(bitmap, -32, -26, body, GraphicsUnit.Pixel);
             g.ResetTransform();
 
             //Тень башни
-            g.TranslateTransform(position.X + shadow / 2, position.Y + shadow / 2);
+            g.TranslateTransform(position.X + shadow/2, position.Y + shadow/2);
             g.RotateTransform(vectorTower);
             g.DrawImage(bitmap, -32, -26, towerShadow, GraphicsUnit.Pixel);
             g.ResetTransform();
