07dd33237cc5f8fa1dd970ccd7a749d394b8b137
Sergey74ua <Sergey74ua@gmail.com>
2020-04-26 15:05:06 +0300

    Update Shots.cs

--- C#/Tanks/Units/Shots.cs
+++ C#/Tanks/Units/Shots.cs
@@ -11,22 +11,15 @@ namespace Tanks
         //Добавляем выстрел + залп
         public void NewShot(PointF position, PointF target, Color party) //********
         {
+            float vector = (float)Math.Atan2(target.Y - position.Y, target.X - position.X);
             ListShot.Add(new Shot
             {
                 party = party,
                 position = position,
                 target = target,
-                vector = Vector(position, target),
-                speed = 24.0f
+                vector = vector,
+                speed = 16.0f
             });
-        }
-
-        //Определяем угол на цель
-        private float Vector(PointF position, PointF target)
-        {
-            float vector = (float)Math.Atan2(target.Y - position.Y, target.X - position.X);
-
-            return vector;
         }
 
         //Отрисовываем выстрелы по списку
@@ -37,9 +30,11 @@ namespace Tanks
                 ListShot[i].DrawShot(g);
 
                 //Удаляем снаряды на финише
-                if (Math.Abs(ListShot[i].position.X - ListShot[i].target.X) < ListShot[i].speed &&
-                    Math.Abs(ListShot[i].position.Y - ListShot[i].target.Y) < ListShot[i].speed)
+                if (ListShot[i].speed < 1 ||
+                    (Math.Abs(ListShot[i].position.X - ListShot[i].target.X) < ListShot[i].speed &&
+                    Math.Abs(ListShot[i].position.Y - ListShot[i].target.Y) < ListShot[i].speed))
                 {
+                    //передаем координаты в функцию взрыва
                     ListShot.Remove(ListShot[i]);
                 }
             }
