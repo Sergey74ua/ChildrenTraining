406bea4d03a7d6570e6308a69896d60d0b0b2948
Sergey74ua <Sergey74ua@gmail.com>
2020-04-26 15:04:56 +0300

    Update Party.cs

--- C#/Tanks/Units/Party.cs
+++ C#/Tanks/Units/Party.cs
@@ -6,19 +6,21 @@ namespace Tanks
 {
     class Party
     {
-        public List<object> ListUnits = new List<object>();
+        private readonly Size window = Tanks.Window;
         private Random random = new Random();
 
+        public List<object> ListUnits = new List<object>();
+
         //Заполняем список юнитами
-        public List<object> CreateListUnits(Color party, byte count, Size Window)
+        public List<object> CreateListUnits(Color party, byte count)
         {
-            PointF position;    //случайная позиция
-            float vector;       //поворот на центр
+            PointF position; //случайная позиция
+            float vector; //поворот на центр
 
             for (byte i = 0; i < count; i++)
             {
-                position = Start(party, Window);
-                vector = Vector(position, Window);
+                position = Start(party);
+                vector = Vector(position);
                 ListUnits.Add(new Tank
                 {
                     party = party,
@@ -29,8 +31,8 @@ namespace Tanks
                     life = 40
                 });
 
-                position = Start(party, Window);
-                vector = Vector(position, Window);
+                position = Start(party);
+                vector = Vector(position);
                 ListUnits.Add(new Car
                 {
                     party = party,
@@ -46,22 +48,22 @@ namespace Tanks
         }
 
         //Начальная случайная позиция
-        private PointF Start(Color party, Size Window)
+        private PointF Start(Color party)
         {
             PointF point = PointF.Empty;
             if (party == Color.DarkBlue)
-                point.X = random.Next(50, Window.Width/2-200);
+                point.X = random.Next(50, window.Width/2-200);
             if (party == Color.DarkRed)
-                point.X = random.Next(Window.Width/2+200, Window.Width-50);
-            point.Y = random.Next(50, Window.Height-50);
+                point.X = random.Next(window.Width/2+200, window.Width-50);
+            point.Y = random.Next(50, window.Height-50);
 
             return point;
         }
 
         //Начальный угол на центр (можно заменить на 90°/270°)
-        private float Vector(PointF position, Size Window)
+        private float Vector(PointF position)
         {
-            float vector = (float)(Math.Atan2(Window.Height/2 - position.Y, Window.Width/2 - position.X)*180/Math.PI+90);
+            float vector = (float)(Math.Atan2(window.Height/2 - position.Y, window.Width/2 - position.X)*180/Math.PI+90);
             if (vector < 0) vector += 360;
 
             return vector;
@@ -72,13 +74,15 @@ namespace Tanks
         {
             foreach (dynamic unit in ListUnits)
             {
-                if (unit.timeShot <= 0)
+                if (unit.timeShot >= 180)
                 {
                     AllShots.NewShot(unit.position, unit.target, unit.party); //******** пробно ********
-                    unit.timeShot = 120; //******** пробно ********
+                    unit.timeShot = 0; //******** пробно ********
                 }
-                unit.timeShot--;
+                unit.timeShot++;
+
                 unit.target = cursor; //должна быть ссылка на функцию определения таргета
+
                 unit.DrawUnit(g, unit.party);
             }
         }
