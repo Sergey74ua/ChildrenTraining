39f08982fc0e3d7d7056adb391ac8fa7240251cd
Sergey74ua <Sergey74ua@gmail.com>
2020-05-14 22:55:34 +0300

    Create Craters.cs

---
+++ C#/Tanks/GameObject/Lists/Craters.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Tanks
+{
+    class Craters
+    {
+        public List<PointF> listCraters = new List<PointF>();
+
+        public PointF position; //позиция
+        private byte timeCraters; //таймер взрыва
+
+        //Добавляем воронку в список
+        public void NewCrater()
+        {
+            //********
+        }
+
+        //Удаляем воронку из списка
+        public void RemoveCrater()
+        {
+            //********
+        }
+
+        public void DrawListCraters(Graphics g)
+        {
+            //Отрисовка взрыва
+            if (timeCraters < 96)
+            {
+                timeCraters += 8;
+                g.TranslateTransform(position.X, position.Y);
+                g.FillEllipse(new SolidBrush(Color.FromArgb(192 - timeCraters, timeCraters + 128, timeCraters, 0)),
+                    new RectangleF(-timeCraters / 2, -timeCraters / 2, timeCraters, timeCraters));
+                g.ResetTransform();
+            }
+        }
+    }
+}
