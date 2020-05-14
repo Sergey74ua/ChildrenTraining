95d60c345a3b279fa45290b12e23250ecefa14cc
Sergey74ua <Sergey74ua@gmail.com>
2020-05-14 22:55:52 +0300

    Create Sound.cs

---
+++ C#/Tanks/Resources/Sound.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Tanks
+{
+    class Sound
+    {
+        //Музыкальная заставка
+        async public static void StarWars()
+        {
+            await Task.Run(() =>
+            {
+                Console.Beep(440, 500);
+                Console.Beep(440, 500);
+                Console.Beep(440, 500);
+                Console.Beep(349, 350);
+                Console.Beep(523, 150);
+                Console.Beep(440, 500);
+                Console.Beep(349, 350);
+                Console.Beep(523, 150);
+                Console.Beep(440, 1000);
+                Console.Beep(659, 500);
+                Console.Beep(659, 500);
+                Console.Beep(659, 500);
+                Console.Beep(698, 350);
+                Console.Beep(523, 150);
+                Console.Beep(415, 500);
+                Console.Beep(349, 350);
+                Console.Beep(523, 150);
+                Console.Beep(440, 1000);
+            });
+        }
+    }
+}
