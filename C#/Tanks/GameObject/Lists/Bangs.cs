a34d81587590ba105e45cfb42a52f497ff45c2ec
Sergey74ua <Sergey74ua@gmail.com>
2020-05-14 22:55:33 +0300

    Create Bangs.cs

---
+++ C#/Tanks/GameObject/Lists/Bangs.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Tanks
+{
+    class Bangs
+    {
+        public List<Bang> listBang = new List<Bang>();
+
+        //Добавляем взрыв в список
+        public void NewBang()
+        {
+            //********
+        }
+
+        //Удаляем взрыв из списка
+        public void RemoveBang()
+        {
+            //********
+        }
+
+        //Отрисовывка сиска взрывов
+        public void DrawListBangs(Graphics g)
+        {
+            foreach (Bang bang in listBang)
+            {
+                bang.DrawBang(g);
+            }
+        }
+    }
+}
