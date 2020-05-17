7a13d17a69ae5852ed39bb9dec2f00a11988bf7d
Sergey74ua <Sergey74ua@gmail.com>
2020-05-17 11:27:48 +0300

    Create Sphere.cs

---
+++ C#/Tanks/Object/Sphere.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace Tanks
+{
+    class Sphere
+    {
+        public PointF position;    //позиция
+        public Color color;        //цвет сферы
+        public byte timeAction;    //время действия
+
+        public Sphere() //********
+        {
+            timeAction = 128;
+        }
+
+        //Отрисовывка взрыва
+        public void DrawSphere(Graphics g)
+        {
+            g.TranslateTransform(position.X, position.Y);
+            g.FillEllipse(new SolidBrush(color), new RectangleF(-timeAction / 2, -timeAction / 2, timeAction, timeAction));
+            g.ResetTransform();
+        }
+    }
+}
