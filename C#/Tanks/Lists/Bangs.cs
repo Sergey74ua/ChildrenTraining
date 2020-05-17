d6bd4fa1007c33c6dbc7a424e7564c2ae0bdbacb
Sergey74ua <Sergey74ua@gmail.com>
2020-05-17 11:27:25 +0300

    Create Bangs.cs

---
+++ C#/Tanks/Lists/Bangs.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Tanks
+{
+    class Bangs
+    {
+        public List<Sphere> ListBang = new List<Sphere>();
+
+        //Добавляем взрыв в список
+        public void NewBang(Shot shot)
+        {
+            ListBang.Add(new Sphere()
+            {
+                position = shot.position,
+                color = shot.party,
+                timeAction = 0
+            });
+        }
+
+        //Удаляем взрыв из списка
+        public void RemoveBang()
+        {
+            //********
+        }
+
+        //Отрисовывка списка взрывов
+        public void DrawListBangs(Graphics g)
+        {
+            foreach (Sphere bang in ListBang)
+            {
+                bang.DrawSphere(g);
+            }
+        }
+    }
+}
