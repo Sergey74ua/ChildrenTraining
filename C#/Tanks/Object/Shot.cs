5e90e2083074108c291221656f446cd5be166df3
Sergey74ua <Sergey74ua@gmail.com>
2020-05-17 11:27:46 +0300

    Create Shot.cs

---
+++ C#/Tanks/Object/Shot.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+
+namespace Tanks
+{
+    class Shot : AObject
+    {
+        public uint timeShot; //перезарядка
+
+        private Pen pen;
+        private PointF _position;
+
+        //Конструктор
+        public Shot()
+        {
+            speed = 16.0f;
+        }
+
+        //Отрисовка полета снаряда
+        public void DrawShot(Graphics g)
+        {
+            _position = position;
+            position = Position();
+            speed *= 0.98f; //затухание скорости
+            pen = new Pen(party, 3);
+
+            g.DrawLine(pen, position, _position);
+        }
+    }
+}
