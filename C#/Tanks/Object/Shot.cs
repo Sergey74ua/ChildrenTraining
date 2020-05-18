449e2794f012140e354b5880a0f322301210459e
Sergey74ua <Sergey74ua@gmail.com>
2020-05-18 19:12:44 +0300

    Update Shot.cs

--- C#/Tanks/Object/Shot.cs
+++ C#/Tanks/Object/Shot.cs
@@ -1,19 +1,41 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 
 namespace Tanks
 {
     class Shot : AObject
     {
-        public uint timeShot; //перезарядка
+        public byte timeShot;       //перезарядка
 
-        private Pen pen;
-        private PointF _position;
+        private PointF _position;   //хвост снаряда
+        private Color color;        //цвет снаряда
+        private Pen pen;            //перо для снаряда (для пулеметов можно сделать другой)
 
-        //Конструктор
-        public Shot()
+        /// <summary>
+        /// Выстрел: рассчитывается из объекта.
+        /// </summary>
+        public Shot(dynamic unit)
         {
+            party = ColorShot(unit.party);
+            position = unit.position;
+            target = unit.target;
+            vector = (float)Math.Atan2(target.Y - position.Y, target.X - position.X);
             speed = 16.0f;
         }
+
+        //Цвет выстрела
+        private Color ColorShot(Color party)
+        {
+            color = Color.FromArgb
+            (
+                party.R + (255 - party.R) / 4,
+                party.G + (255 - party.G) / 8,
+                party.B + (255 - party.B) / 4
+            );
+
+            return color;
+        }
+
 
         //Отрисовка полета снаряда
         public void DrawShot(Graphics g)
