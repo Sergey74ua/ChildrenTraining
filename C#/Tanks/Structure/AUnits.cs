5db0708cafc89743b78b20191f296ac1e92280fc
Sergey74ua <Sergey74ua@gmail.com>
2020-05-22 01:17:34 +0300

    Update AUnits.cs

--- C#/Tanks/Structure/AUnits.cs
+++ C#/Tanks/Structure/AUnits.cs
@@ -10,20 +10,16 @@ namespace Tanks
         //Общие поля юнитов
         public uint id = ++ID;      //имя
         public Act act;             //действие
-        public float life;          //жизнь
+        public float life, lifeLine;//жизнь
         public float vectorTower;   //вектор башни
-        public uint timeAction;     //******** перезарядка (проба)
-        public bool Atack;          //********
-        //public bool Select;         //Выбор
-
-        private float angle, gipotenuza;
+        public object targetID;     //******** атакуемая цель (проба) ********
 
         //Данные для отисовки
         private readonly SolidBrush solidBrushFont = new SolidBrush(Color.LightGreen);
         private readonly Font font = new Font("Arial", 10, FontStyle.Bold, GraphicsUnit.Point);
-        private readonly Pen penGrren = new Pen(Color.Green, 2);
+        private readonly Pen penGrn = new Pen(Color.Green, 2);
         private readonly Pen penRed = new Pen(Color.Red, 2);
-        public float lifeLine, line = 64.0f; //********
+        private float angle, line = 64.0f;
 
         //Отрисовка имени и жизни
         protected void DrawInfo(Graphics g)
@@ -31,27 +27,34 @@ namespace Tanks
             //Наименование и полоса жизни
             g.TranslateTransform(position.X, position.Y);
             g.DrawString("= " + id.ToString() + " =", font, solidBrushFont, -20, -42);
-            g.DrawLine(penGrren, -line/2, -26, lifeLine, -26);
-            g.DrawLine(penRed, lifeLine, -26, line/2, -26);
+            g.DrawLine(penGrn, -line / 2, -26, lifeLine, -26);
+            g.DrawLine(penRed, lifeLine, -26, line / 2, -26);
             g.ResetTransform();
         }
 
-        //Направление юнита
-        public float Vector(float vector, float speed)
+        //Движения юнита к цели
+        public void Move()
         {
-            float catetX = target.X - position.X;
-            float catetY = target.Y - position.Y;
+            vector = Angle(vector, speed);
+            vectorTower = Angle(vectorTower, speed * 2);
 
+            if (vector == angle)
+                position = Position();
+        }
+
+        //Направление юнита
+        public float Angle(float vector, float speed)
+        {
             //Расстояние и угол к цели
-            gipotenuza = (float)Math.Sqrt(catetX*catetX + catetY*catetY);
-            angle = (float)(Math.Atan2(catetY, catetX) * 180/Math.PI+90);
+            delta = Delta(target);
+            angle = (float)(Vector() * 180 / Math.PI + 90);
             if (angle < 0) angle += 360;
 
             //Текущий угол поворота к цели
             if (Math.Abs(vector - angle) > speed)
             {
                 if ((vector < angle && (angle - vector) < 180) ^ (angle - vector) > -180)
-                    vector = (vector - speed+360) % 360;
+                    vector = (vector - speed + 360) % 360;
                 else
                     vector = (vector + speed) % 360;
             }
@@ -61,17 +64,12 @@ namespace Tanks
             return vector;
         }
 
-        //Перемещение юнита
-        public PointF Position(PointF position)
+        //Уничтожение юнита
+        public void RemoveUnit(dynamic unit)
         {
-            if (gipotenuza > 128 && vector == angle)
-            {
-                position = Position();
-                if (gipotenuza < 512 && timeAction >= 180)
-                    Atack = true;
-            }
-
-            return position;
+            unit.life = 0.0f;
+            unit.speed = 0.0f;
+            unit.color = Color.Black;
         }
     }
 }
