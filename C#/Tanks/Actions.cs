201088e5c0ba3fcb7da2fb7250b536cf25980f17
Sergey74ua <Sergey74ua@gmail.com>
2020-05-22 01:16:38 +0300

    Update Actions.cs

--- C#/Tanks/Actions.cs
+++ C#/Tanks/Actions.cs
@@ -1,92 +1,167 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Tanks
 {
-    sealed class Actions
+    class Actions
     {
+        private List<Party> ListParty;
+        private Shots ListShots;
+        private Random random = new Random();
+        private readonly byte range = 64;
+        private float delta, _delta;
+
         //Перебор списков всех юнитов
-        public void Step(List<Party> ListParty)
+        public void ActObject(List<Party> ListParty, Shots ListShots)
         {
+            this.ListParty = ListParty;
+            this.ListShots = ListShots;
+
+            //Перерасчет юнитов
             foreach (Party party in ListParty)
                 foreach (dynamic unit in party.ListUnits)
-                    SwitchAct(unit);
+                    SelectAct(unit);
+
+            //Перерасчет выстрелов
+            foreach (Shot shot in ListShots.ListShot)
+            {
+                shot.timeAction++;
+                shot.Move();
+                if (shot.speed < 1 || shot.Delta(shot.target) < shot.speed)
+                    ListShots.RemoveShot(shot);
+            }
+
+            //Перерасчет взрывов
+            for (int i = 0; i < ListShots.ListBang.Count; i++)
+            {
+                //рассчет дамажа ******** НЕ ВЕРНО РАССЧИТЫВАЕТСЯ ДАМАЖ ********
+                if (ListShots.ListBang[i].timeAction > 96)
+                {
+                    foreach (Party party in ListParty)
+                        foreach (dynamic unit in party.ListUnits)
+                            if (unit.Delta(ListShots.ListBang[i].position) < 48 && unit.life > 0)
+                                unit.life -= 10 / unit.Delta(unit.target);
+
+                    //удаляем взрыв
+                    ListShots.RemoveBang(ListShots.ListBang[i]);
+                }
+                else
+                    //процесс взрыва
+                    ListShots.ListBang[i].timeAction += 8;
+            }
+
+            //Перерасчет воронок
+            for (int i = 0; i < ListShots.ListCrater.Count; i++)
+            {
+                ListShots.ListCrater[i].timeAction++;
+                if (ListShots.ListCrater[i].timeAction > 600)
+                    ListShots.RemoveCrater(ListShots.ListCrater[i]);
+            }
         }
 
         //Выбор действия
-        private void SwitchAct(dynamic unit)
+        private void SelectAct(dynamic unit)
         {
             switch (unit.act)
             {
-                case Act.BURN: //танк убит
-                    unit.timeAction++;
+                case Act.DEAD:  //подбит
                     break;
-
-                case Act.WAIT: //ожидание
+                case Act.WAIT:  //ожидание
                     ActWait(unit);
                     break;
-
-                case Act.BACK: //отступление
-                    ActBack(unit);
-                    break;
-
-                case Act.FIRE: //атака
+                case Act.FIRE:  //атака
                     ActFire(unit);
                     break;
-
-                case Act.FIND: //поиск цели
+                case Act.FIND:  //поиск
                     ActFind(unit);
                     break;
-
-                default: //бездействие по умолчанию
+                default:        //прочее
                     unit.act = Act.WAIT;
                     break;
             }
         }
 
-        //При бездейсвии танка
+        //Определяем действие танка
         private void ActWait(dynamic unit)
         {
-            if (unit.life <= 0)
-                unit.act = Act.BURN;
-            if (unit.life <= 2)
+            //если танк убит
+            if (unit.life <= 0.0f)
             {
-
-                unit.act = Act.WAIT;
+                unit.RemoveUnit(unit);
+                unit.act = Act.DEAD;
             }
-            //если ХП < 10% то Act.BACK - методы отката и ремонта
-            //если есть живая цель Act.FIRE - методы поворот, подкат и выстрел
-            //если цели нет, искать цель - поворот и подкат на рандомную точку
-            //поиск целт (если надо)
+
+            //если у танка есть живая цель
+            else if (unit.targetID != null)
+            {
+                if (unit.delta < 512 && unit.timeAction >= 180)
+                    unit.Atack = true;
+                unit.act = Act.FIRE;
+            }
+
+            //поиск цели, если ее нет
+            else if (unit.targetID == null)
+            {
+                foreach (Party party in ListParty)
+                    foreach (dynamic _unit in party.ListUnits)
+                    {
+                        //поиск цели
+                        if (unit.color != _unit.color && _unit.life > 0)
+                        {
+                            //отбор ближайшей цели (точно работает?)
+                            _delta = unit.Delta(_unit.position);
+                            if (delta > _delta)
+                            {
+                                delta = _delta;
+                                unit.targetID = _unit;
+                                unit.target = _unit.position;
+                                unit.act = Act.FIRE;
+                            }
+                            else
+                                delta = 756;
+                        }
+                    }
+            }
+
+            //повторяем если цель убита или не найдена ********  Н Е   Р А Б О Т А Е Т  ********
+            else
+            {
+                unit.target.X = unit.position.X + random.Next(-range, range);
+                unit.target.Y = unit.position.Y + random.Next(-range, range);
+                unit.act = Act.FIND;
+            }
         }
 
-        //Отступление танка
-        private void ActBack(dynamic unit)
-        {
-            if (unit.target == null) //проверка на отсутствие врага **** не таргет ****
-                unit.life += 0.05f;
-            //иначе - рассчет точки отката
-            //откат
-            //обзор
-            if (unit.life >= 40)
-                unit.act = Act.WAIT;
-        }
-
-        //Атака танка
+        //Процесс атаки танка
         private void ActFire(dynamic unit)
         {
-            //если направление и дистанция позволяет - стоять и стрелять
-            //рассчет дамажа
-            //иначе - поворот на цель
-            //и движение к цели
-            //а если цель убита - ActWait
+            if (unit.Delta(unit.targetID.position) < 256 && unit.timeAction > 60)
+            {
+                ListShots.NewShot(unit);
+                unit.targetID = null;
+                unit.timeAction = 0;
+                unit.act = Act.WAIT;
+            }
+            else
+            {
+                unit.timeAction++;
+                unit.Move();
+            }
         }
 
-        //Поиск в отсутствии цели
+        //Процесс поиска цели
         private void ActFind(dynamic unit)
         {
-            //выбор рандомной точки
-            //поворот на точку
-            //и движение к точке
+            if (unit.Delta(unit.target) < 128 && unit.timeAction < 60) //******** тут все нормально? ********
+            {
+                unit.timeAction++;
+                unit.Move(); Console.WriteLine(unit.Delta(unit.target).ToString()); //****
+            }
+            else
+            {
+                unit.timeAction = 0;
+                unit.act = Act.WAIT;
+            }
         }
     }
 }
