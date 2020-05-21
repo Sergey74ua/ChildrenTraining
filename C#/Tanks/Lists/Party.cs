ac2428d1a2125598abbc49d3216816882293f17a
Sergey74ua <Sergey74ua@gmail.com>
2020-05-22 01:17:03 +0300

    Update Party.cs

--- C#/Tanks/Lists/Party.cs
+++ C#/Tanks/Lists/Party.cs
@@ -7,68 +7,48 @@ namespace Tanks
     class Party
     {
         public List<object> ListUnits = new List<object>();
-        private readonly int width = Tanks.Window.Width;
         private readonly int height = Tanks.Window.Height;
+        private readonly int width = Tanks.Window.Width;
         private readonly Random random = new Random();
+        private ushort range; //разброс на старте
 
-        /// <summary>
-        /// Команда: цвет флага, стартовая позиция(X,Y) в %, число танков, число машин.
-        /// </summary>
-        public Party(Color party, Point start, byte tank, byte car)
+        /// <summary> Команда : цвет флага, стартовая позиция(X,Y) в %, число танков, число машин. </summary>
+        public Party(Color color, Point start, byte tank, byte car)
         {
             for (byte i = 0; i < tank; i++)
-                NewUnit(new Tank(party), start);
+                NewUnit(new Tank(color), start);
 
             for (byte i = 0; i < car; i++)
-                NewUnit(new Car(party), start);
+                NewUnit(new Car(color), start);
         }
 
         //Отрисовываем юнитов по списку
-        public void DrawListUnits(Graphics g, Shots ListShots, Point cursor)
+        public void DrawListUnits(Graphics g)
         {
             foreach (dynamic unit in ListUnits)
-            {
-                unit.timeAction++; //******** пробно ********
-                if (unit.Atack) //******** пробно ********
-                {
-                    ListShots.NewShot(unit);
-                    unit.timeAction = 0;
-                    unit.Atack = false;
-                }
-                unit.target = cursor; //должна быть ссылка на функцию определения таргета (см. выше или у Game)
-
-                unit.DrawUnit(g, unit.party);
-            }
+                unit.DrawUnit(g, unit.color);
         }
 
         //Добавляем юнита в список
         private void NewUnit(dynamic unit, Point start)
         {
-            byte delta = 16;
             ListUnits.Add(unit);
+            unit.target = new Point(width / 2, height / 2);
             unit.position = Start(start);
-            unit.vector = Vector(unit.position) + random.Next(-delta, delta);
-            unit.vectorTower = unit.vector + random.Next(-delta, delta);
+            unit.vector = (float)(unit.Vector() * 180 / Math.PI + 90) + random.Next(-16, 16);
+            if (unit.vector < 0)
+                unit.vector += 360;
+            unit.vectorTower = unit.vector + random.Next(-16, 16);
         }
 
         //Начальная случайная позиция
         private PointF Start(Point point)
         {
-            ushort delta = 128;
-            point.X = width * point.X / 100 + random.Next(-delta, delta);
-            point.Y = height * point.Y / 100 + random.Next(-delta, delta);
+            range = 128;
+            point.X = width * point.X / 100 + random.Next(-range, range);
+            point.Y = height * point.Y / 100 + random.Next(-range, range);
 
             return point;
-        }
-
-        //Начальный угол на центр (можно заменить на 90°/270°)
-        private float Vector(PointF position)
-        {
-            float vector = (float)(Math.Atan2(height/2 - position.Y,
-                width/2 - position.X)*180/Math.PI+90);
-            if (vector < 0) vector += 360;
-
-            return vector;
         }
     }
 }
