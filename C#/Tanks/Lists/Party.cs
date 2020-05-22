97c3c8fb2dee5b2aa99efeb2889112973bc64a55
Sergey74ua <Sergey74ua@gmail.com>
2020-05-22 21:36:42 +0300

    Update Party.cs

--- C#/Tanks/Lists/Party.cs
+++ C#/Tanks/Lists/Party.cs
@@ -12,14 +12,36 @@ namespace Tanks
         private readonly Random random = new Random();
         private ushort range; //разброс на старте
 
+        /// <summary> Команда : без параметров (случайно). </summary>
+        public Party()
+        {
+            Color color = Color.FromArgb(255, Color.FromArgb(random.Next(0xFFFFFF + 1)));
+            Point start = new Point(random.Next(80) + 10, random.Next(80) + 10);
+            byte unit = (byte)random.Next(8);
+            range = 128;
+            NewParty(color, start, unit, unit);
+        }
+
+        /// <summary> Команда : цвет флага, число танков/машин. </summary>
+        public Party(Color color, byte unit)
+        {
+            Point start = new Point(random.Next(80)+10, random.Next(80)+10);
+            range = 256;
+            NewParty(color, start, unit, unit);
+        }
+
+        /// <summary> Команда : цвет флага, стартовая позиция(X,Y) в %, число танков/машин. </summary>
+        public Party(Color color, Point start, byte unit)
+        {
+            range = 128;
+            NewParty(color, start, unit, unit);
+        }
+
         /// <summary> Команда : цвет флага, стартовая позиция(X,Y) в %, число танков, число машин. </summary>
         public Party(Color color, Point start, byte tank, byte car)
         {
-            for (byte i = 0; i < tank; i++)
-                NewUnit(new Tank(color), start);
-
-            for (byte i = 0; i < car; i++)
-                NewUnit(new Car(color), start);
+            range = 128;
+            NewParty(color, start, tank, car);
         }
 
         //Отрисовываем юнитов по списку
@@ -27,6 +49,16 @@ namespace Tanks
         {
             foreach (dynamic unit in ListUnits)
                 unit.DrawUnit(g, unit.color);
+        }
+
+        //Новая команда
+        private void NewParty(Color color, Point start, byte tank, byte car)
+        {
+            for (byte i = 0; i < tank; i++)
+                NewUnit(new Tank(color), start);
+
+            for (byte i = 0; i < car; i++)
+                NewUnit(new Car(color), start);
         }
 
         //Добавляем юнита в список
@@ -44,7 +76,8 @@ namespace Tanks
         //Начальная случайная позиция
         private PointF Start(Point point)
         {
-            range = 128;
+            if (range <= 0)
+                range = 128;
             point.X = width * point.X / 100 + random.Next(-range, range);
             point.Y = height * point.Y / 100 + random.Next(-range, range);
 
