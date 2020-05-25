e248d321341416588285ad125c78af6242229026
Sergey74ua <Sergey74ua@gmail.com>
2020-05-25 20:22:10 +0300

    Create AI.cs

---
+++ C#/Tanks/Script/AI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tanks
+{
+    class AI
+    {
+        private Random random = new Random();
+        private byte ai;
+
+        public byte StepFind()
+        {
+            ai = (byte)random.Next(byte.MaxValue);
+
+            return ai;
+        }
+    }
+}
