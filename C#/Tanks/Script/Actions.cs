9bc45af37716ca349111aff16fbc6e134c7ced19
Sergey74ua <Sergey74ua@gmail.com>
2020-05-27 23:44:20 +0300

    Update Actions.cs

--- C#/Tanks/Script/Actions.cs
+++ C#/Tanks/Script/Actions.cs
@@ -6,74 +6,40 @@ namespace Tanks
     class Actions
     {
         private readonly Random random = new Random();
-        private float delta, _delta;
-        private AI ai = new AI();
 
         private List<Party> ListParty;
         private Shots ListShots;
 
-        //Перебор всех объектов игры
-        public void ActObject(List<Party> ListParty, Shots ListShots)
+        //Перебор всех юнитов
+        public void ActUnit(List<Party> ListParty, Shots ListShots)
         {
             this.ListParty = ListParty;
             this.ListShots = ListShots;
 
-            //Перерасчет юнитов
+            //Перебор действий юнитов
             foreach (Party party in ListParty)
                 foreach (dynamic unit in party.ListUnits)
-                    SelectAct(unit);
-
-            //Перерасчет выстрелов
-            foreach (Shot shot in ListShots.ListShot)
-            {
-                shot.Move();
-                if (shot.speed < 2 || shot.Delta(shot.target) < shot.speed)
-                    ListShots.RemoveShot(shot);
-            }
-
-            //Перерасчет взрывов
-            for (int i = 0; i < ListShots.ListBang.Count; i++)
-            {
-                //рассчет дамажа ******** НЕ ВЕРНО РАССЧИТЫВАЕТСЯ ДАМАЖ ********
-                if (ListShots.ListBang[i].timeAction > 96)
-                {
-                    foreach (Party party in ListParty)
-                        foreach (dynamic unit in party.ListUnits)
-                            if (unit.Delta(ListShots.ListBang[i].position) < 48 && unit.life > 0)
-                                unit.life -= 10 / unit.Delta(unit.target);
-
-                    //удаляем взрыв
-                    ListShots.RemoveBang(ListShots.ListBang[i]);
-                }
-                else
-                    //процесс взрыва
-                    ListShots.ListBang[i].timeAction += 8;
-            }
-
-            //Перерасчет воронок
-            for (int i = 0; i < ListShots.ListCrater.Count; i++)
-            {
-                ListShots.ListCrater[i].timeAction++;
-                if (ListShots.ListCrater[i].timeAction > 600)
-                    ListShots.RemoveCrater(ListShots.ListCrater[i]);
-            }
+                    if (unit.act != Act.DEAD)
+                        SelectAct(unit);
         }
 
-        //Выбор действия
+        //Логика действий
         private void SelectAct(dynamic unit)
         {
+            unit.timeAction++;
             switch (unit.act)
             {
-                case Act.DEAD:  //подбит
-                    break;
                 case Act.WAIT:  //ожидание
                     ActWait(unit);
                     break;
-                case Act.FIRE:  //атака
-                    ActFire(unit);
-                    break;
                 case Act.FIND:  //поиск
                     ActFind(unit);
+                    break;
+                case Act.MOVE:  //подкат
+                    ActMove(unit);
+                    break;
+                case Act.FIRE:  //атака
+                    ActFire(unit);
                     break;
                 default:        //прочее
                     unit.act = Act.WAIT;
@@ -84,59 +50,59 @@ namespace Tanks
         //Определяем действие танка
         private void ActWait(dynamic unit)
         {
-            //если танк убит
-            if (unit.life <= 0.0f)
-            {
-                unit.RemoveUnit(unit);
-                unit.act = Act.DEAD;
-            }
+            unit.timeAction = 0;
 
-            //если у танка есть живая цель
-            else if (unit.targetID != null)
+            //DEAD - если танк убит ******** баг - будет доезжать мертвым ********
+            if (unit.life <= 0.0f)
+                unit.RemoveUnit(unit);
+
+            else if (unit.targetID == null)
             {
-                if (unit.Delta(unit.target) < 384 && unit.timeAction >= 180)
+                unit.targetID = unit.FindUnit(ListParty);
+                
+                //FIND - поиск цели и случайный перекат если ее нет
+                if (unit.targetID == null)
+                    unit.act = Act.FIND;
+
+                //MOVE - движемся в направлении цели
+                else if (unit.targetID != null && unit.Delta(unit.target) > 256 || unit.timeAction < 60)
+                    unit.act = Act.MOVE;
+
+                //FIRE - атака живой цели в зоне поражения
+                else if(unit.targetID != null && unit.Delta(unit.target) < 256 && unit.timeAction > 60)
                     unit.act = Act.FIRE;
             }
 
-            //поиск цели, если ее нет
-            else if (unit.targetID == null)
-            {
-                foreach (Party party in ListParty)
-                    foreach (dynamic _unit in party.ListUnits)
-                    {
-                        //поиск цели
-                        if (unit.color != _unit.color && _unit.life > 0)
-                        {
-                            //отбор ближайшей цели (точно работает?)
-                            _delta = unit.Delta(_unit.position);
-                            if (delta > _delta)
-                            {
-                                delta = _delta;
-                                unit.targetID = _unit;
-                                unit.target = _unit.position;
-                                unit.act = Act.FIRE;
-                            }
-                            else
-                                delta = 756;
-                        }
-                    }
-            }
-
-            //повторяем если цель убита или не найдена ********  Н Е   Р А Б О Т А Е Т  ********
+            //WAIT - повторяем в случае сбоя
             else
-            {
-                if (unit.ai <= 0)
-                    unit.ai = ai.StepFind();
-                unit.target.X = unit.position.X + random.Next(-unit.ai, unit.ai);
-                unit.target.Y = unit.position.Y + random.Next(-unit.ai, unit.ai);
-                unit.act = Act.FIND;
-            }
+                unit.act = Act.WAIT;
         }
-
+        //*********************************************************************************************************************
+        //Процесс движения в случаную точку при отсутствии цели
+        private void ActFind(dynamic unit)
+        {
+            if (unit.Delta(unit.target) > 16)
+                unit.Move();
+            else
+                unit.act = Act.WAIT;
+        }
+        //*********************************************************************************************************************
+        //Процесс сближения с целью
+        private void ActMove(dynamic unit)
+        {
+            if (unit.Delta(unit.target) > 256 || unit.timeAction < 60)
+                unit.Move();
+            else if (unit.targetID != null)
+                unit.act = Act.FIRE;
+            else
+                unit.act = Act.WAIT;
+        }
+        //*********************************************************************************************************************
         //Процесс атаки танка
         private void ActFire(dynamic unit)
         {
-            if (unit.Delta(unit.targetID.position) < 256 && unit.timeAction > 180)
+            unit.target = unit.targetID.position;
+            if (unit.Delta(unit.target) < 256 && unit.timeAction > 180)
             {
                 ListShots.NewShot(unit);
                 unit.targetID = null;
@@ -144,25 +110,7 @@ namespace Tanks
                 unit.act = Act.WAIT;
             }
             else
-            {
-                unit.timeAction++;
-                unit.Move();
-            }
-        }
-
-        //Процесс поиска цели
-        private void ActFind(dynamic unit)
-        {
-            if (unit.Delta(unit.target) < 128) //******** тут все нормально? ********
-            {
-                unit.timeAction++;
-                unit.Move();
-            }
-            else
-            {
-                unit.timeAction = 0;
-                unit.act = Act.WAIT;
-            }
+                unit.vectorTower = unit.Angle(unit.vectorTower, unit.speed * 2);
         }
     }
 }
