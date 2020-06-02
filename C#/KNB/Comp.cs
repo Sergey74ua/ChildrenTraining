f8bebed300cac0cfc8592a8a103dd5ec0c99e2b1
Sergey74ua <Sergey74ua@gmail.com>
2020-06-02 22:05:28 +0300

    Create Comp.cs

---
+++ C#/KNB/Comp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace KNB
+{
+    class Comp
+    {
+        public byte comp;
+
+        private Random random = new Random();
+
+        public byte StepComp()
+        {
+            comp = (byte)random.Next(3);
+
+            return comp;
+        }
+    }
+}
