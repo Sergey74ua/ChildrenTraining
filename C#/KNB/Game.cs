3dadbd0e65f94e664e40e7e66ef91e6c0264b00e
Sergey74ua <Sergey74ua@gmail.com>
2020-06-02 22:05:26 +0300

    Create Game.cs

---
+++ C#/KNB/Game.cs
@@ -0,0 +1,34 @@
+﻿namespace KNB
+{
+    class Game
+    {
+        public int win, lose, draw;
+
+        private Comp comp = new Comp();
+        public string resGame;
+        public byte compHand;
+
+        public void StepGame(byte playerHand)
+        {
+            compHand = comp.StepComp();
+
+            if (playerHand == compHand)
+            {
+                resGame = "НИЧЬЯ";
+                draw++;
+            }
+            else if ((playerHand == 0 && compHand == 1) ||
+                (playerHand == 1 && compHand == 2) ||
+                (playerHand == 2 && compHand == 0))
+            {
+                resGame = "ПОБЕДА";
+                win++;
+            }
+            else
+            {
+                resGame = "ПРОИГРЫШ";
+                lose++;
+            }
+        }
+    }
+}
