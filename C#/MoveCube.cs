55f992de00cb2d32f6e94a3b71c8bc7bad394fea
Sergey74ua <Sergey74ua@gmail.com>
2020-06-18 19:58:16 +0300

    Create MoveCube.cs

---
+++ C#/MoveCube.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject MyCube;
+    float delta;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        delta = Input.GetAxis("Horizontal") * 50;
+
+        MyCube.transform.position = new Vector3(100 + delta, 20, 100);
+
+        //Куб - красный
+        if (Input.GetKeyUp(KeyCode.R))
+            MyCube.GetComponent<Renderer>().material.color = Color.red;
+        //Куб - зеленый
+        else if (Input.GetKeyUp(KeyCode.G))
+            MyCube.GetComponent<Renderer>().material.color = Color.green;
+        //Куб - зеленый
+        else if (Input.GetKeyUp(KeyCode.B))
+            MyCube.GetComponent<Renderer>().material.color = Color.blue;
+
+        //Куб - скрываем
+        if (Input.GetKeyUp(KeyCode.Z) && MyCube != null)
+            MyCube.SetActive(false);
+
+        //Куб - показываем
+        if (Input.GetKeyUp(KeyCode.X) && MyCube != null)
+            MyCube.SetActive(true);
+
+        //Куб - удаляем
+        if (Input.GetKeyUp(KeyCode.Escape))
+            Destroy(MyCube);
+    }
+}
