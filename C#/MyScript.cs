67e11e4522c952169a90546c023b2d9854700d8a
Sergey74ua <Sergey74ua@gmail.com>
2020-06-18 19:58:18 +0300

    Update MyScript.cs

--- C#/MyScript.cs
+++ C#/MyScript.cs
@@ -1,64 +1,37 @@
-﻿using System;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class MyScript : MonoBehaviour
 {
     [SerializeField]
-    private Light MyLight;
     public int x, y;
-
-    public GameObject MyCube;
 
     //Функции обрабатываемые до запуска
     private void Awake()
     {
-        //print("Hello world");
+        print("Hello world");
     }
 
     //Функции обрабатываемые при запуске
     void Start()
     {
-        MyLight = GetComponent<Light>();
+        print("Hello world!");
     }
 
     //Функции обрабатываемые при смене кадра
     void Update()
     {
-        MyFunction();
-
-        //Включаем / выключаем свет
-        if (Input.GetKeyUp(KeyCode.Space))
-            MyLight.enabled = !MyLight.enabled;
-
-        //Куб - скрываем
-        if (Input.GetKeyUp(KeyCode.W) && MyCube != null)
-            MyCube.SetActive(false);
-
-        //Куб - показываем
-        if (Input.GetKeyUp(KeyCode.S) && MyCube != null)
-            MyCube.SetActive(true);
-
-        //Куб - удаляем
-        if (Input.GetKeyUp(KeyCode.Q) && MyCube != null)
-            Destroy (MyCube);
-
-        //Куб - красный
-        if (Input.GetKeyUp(KeyCode.R) && MyCube != null)
-            MyCube.GetComponent <Renderer>().material.color = Color.red;
-
-        //Куб - желтый
-        if (Input.GetKeyUp(KeyCode.E) && MyCube != null)
-            MyCube.GetComponent<Renderer>().material.color = Color.yellow;
+        //MyFunction();
     }
 
+    //Функции обрабатываемые по таймеру
     void FixedUpdate()
     {
-        //print(y++ + " Время " + Time.deltaTime);
+        //print(y++ + " Время по таймеру " + Time.deltaTime);
     }
 
+    //Пользовательская функция
     void MyFunction()
     {
-        print(x++ + " Время " + Time.deltaTime);
+        print(x++ + " Время по кадру " + Time.deltaTime);
     }
 }
