f1e83e2bbc917e06b087ac8040da8086d552a1a4
Sergey74ua <Sergey74ua@gmail.com>
2020-07-14 14:26:16 +0300

    Create Fishes.cs

---
+++ C#/Aquarium/Aquarium/Fishes.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Aquarium
+{
+    class Fishes
+    {
+        public List<object> ListFish = new List<object>();
+
+        private readonly Random random = new Random();
+
+        /// <summary>
+        /// Список рыбок
+        /// </summary>
+        public Fishes(byte count)
+        {
+            for (byte i = 0; i < count; i++)
+            {
+                NewUnit(new FishGold());
+                NewUnit(new FishBlue());
+                NewUnit(new FishRed());
+            }
+        }
+
+        //Добавляем рыбку в список
+        private void NewUnit(dynamic fish)
+        {
+            ListFish.Add(fish);
+            fish.position = RandomPosition();
+            fish.target = RandomPosition();
+            fish.bitmap = fish.Rotate();
+        }
+
+        //Рассчет случайной позиции
+        public PointF RandomPosition()
+        {
+            PointF point = new PointF();
+
+            point.X = random.Next(50, 1770);
+            point.Y = random.Next(50, 930);
+
+            return point;
+        }
+
+        //Отрисовка списка рыбок
+        public void DriweListFish(Graphics g)
+        {
+            foreach (dynamic fish in ListFish)
+            {
+                fish.Position();
+                fish.DrawFish(g);
+            }
+        }
+    }
+}
