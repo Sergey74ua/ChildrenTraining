1eba1cf57158f0c21a8938ddf09721966ea5a790
Sergey74ua <Sergey74ua@gmail.com>
2020-07-14 14:26:09 +0300

    Create AFish.cs

---
+++ C#/Aquarium/Aquarium/AFish.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+
+namespace Aquarium
+{
+    abstract class AFish
+    {
+        public Bitmap bitmap, bitmap2;
+        public PointF position;
+        public PointF target;
+        public float speed;
+
+        private readonly Random random = new Random();
+        private float deltaX, deltaY, vector;
+
+        //Рассчет случайной позиции
+        public PointF RandomPosition()
+        {
+            PointF point = new PointF();
+
+            point.X = random.Next(50, 1770);
+            point.Y = random.Next(50, 930);
+
+            return point;
+        }
+
+        //Рассчет движения
+        public PointF Position()
+        {
+            deltaX = target.X - position.X;
+            deltaY = target.Y - position.Y;
+
+            if (Math.Abs(deltaX) > speed && Math.Abs(deltaY) > speed)
+            {
+                vector = (float)Math.Atan2(deltaY, deltaX);
+                position.X += speed * (float)Math.Cos(vector);
+                position.Y += speed * (float)Math.Sin(vector);
+            }
+            else
+            {
+                target = RandomPosition();
+                bitmap = Rotate();
+            }
+
+            return position;
+        }
+
+        public Bitmap Rotate()
+        {
+            if (position.X > target.X)
+                bitmap.RotateFlip(RotateFlipType.Rotate180FlipY);
+            else
+                bitmap = bitmap2;
+
+            return bitmap;
+        }
+    }
+}
