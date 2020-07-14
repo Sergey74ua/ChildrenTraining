bb3d7dcbe21062051258b74bf66cf8f6b208a9bb
Sergey74ua <Sergey74ua@gmail.com>
2020-07-14 14:26:14 +0300

    Create FishBlue.cs

---
+++ C#/Aquarium/Aquarium/FishBlue.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace Aquarium
+{
+    class FishBlue : AFish, IFish
+    {
+        /// <summary>
+        /// Новая рыбка
+        /// </summary>
+        public FishBlue()
+        {
+            bitmap = bitmap2 = Properties.Resources.FishBlue;
+            bitmap = Rotate();
+            speed = 1.5f;
+        }
+
+        //Отрисовка рыбки
+        public void DrawFish(Graphics g)
+        {
+            g.DrawImage(bitmap, position);
+        }
+    }
+}
