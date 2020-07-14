f5eb298a73c20f9a855318cd8f3546ca3b3ab346
Sergey74ua <Sergey74ua@gmail.com>
2020-07-14 14:26:18 +0300

    Create FishGold.cs

---
+++ C#/Aquarium/Aquarium/FishGold.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace Aquarium
+{
+    class FishGold : AFish, IFish
+    {
+        /// <summary>
+        /// Новая рыбка
+        /// </summary>
+        public FishGold()
+        {
+            bitmap = bitmap2 = Properties.Resources.FishGold;
+            bitmap = Rotate();
+            speed = 1.0f;
+        }
+
+        //Отрисовка рыбки
+        public void DrawFish(Graphics g)
+        {
+            g.DrawImage(bitmap, position);
+        }
+    }
+}
