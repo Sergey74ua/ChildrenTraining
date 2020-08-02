89ffe56180c2896860527832850bb6de27aeda1b
Sergey74ua <Sergey74ua@gmail.com>
2020-08-02 12:03:16 +0300

    Create SwitchLight.cs

---
+++ Unity3D/SwitchLight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class SwitchLight : MonoBehaviour
+{
+    private Light MyLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Включаем / выключаем свет
+        if (Input.GetKeyUp(KeyCode.Space))
+            MyLight.enabled = !MyLight.enabled;
+    }
+}
