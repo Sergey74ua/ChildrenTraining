5833c36b71d3f4cf6aa5797e21af865162206250
Sergey74ua <Sergey74ua@gmail.com>
2020-08-02 12:03:14 +0300

    Create MyScript.cs

---
+++ Unity3D/MyScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    [SerializeField]
+    public int x, y;
+
+    //Функции обрабатываемые до запуска
+    private void Awake()
+    {
+        print("Hello world");
+    }
+
+    //Функции обрабатываемые при запуске
+    void Start()
+    {
+        print("Hello world!");
+    }
+
+    //Функции обрабатываемые при смене кадра
+    void Update()
+    {
+        //MyFunction();
+    }
+
+    //Функции обрабатываемые по таймеру
+    void FixedUpdate()
+    {
+        //print(y++ + " Время по таймеру " + Time.deltaTime);
+    }
+
+    //Пользовательская функция
+    void MyFunction()
+    {
+        print(x++ + " Время по кадру " + Time.deltaTime);
+    }
+}
