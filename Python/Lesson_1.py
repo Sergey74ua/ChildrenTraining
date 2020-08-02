6b7e9348b5eecbdc3da40cce85c148c606630f5b
Sergey74ua <Sergey74ua@gmail.com>
2020-08-02 18:41:02 +0300

    Create Lesson_1.py

---
+++ Python/Lesson_1.py
@@ -0,0 +1,34 @@
+# Урок № 1 по Python
+
+print("1+2+3+4+5")
+print(1+2+3+4+5)
+print("")
+
+x = 5
+print(x)
+print(type(x))
+print("")
+
+x = 5.0
+print(x)
+print(type(x))
+print("")
+
+x = "5.0"
+print(x)
+print(type(x))
+print("строка = " + x)
+print("")
+
+x = 5
+print(x)
+print(type(x))
+print("число = " + str(x))
+print("")
+
+name = input("Как вас зовут?\n")
+print("Очень приятно, ", name)
+
+
+
+input()
