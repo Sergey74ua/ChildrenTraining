be323f89b57348c3d032f56ecf453c58a36f0c11
Sergey74ua <Sergey74ua@gmail.com>
2020-08-06 21:49:09 +0300

    Update Task_01.py

--- Python/Task_01.py
+++ Python/Task_01.py
@@ -1,9 +1,11 @@
 #Високосные года
 import os
 
+os.system("mode con cols=25 lines=3")
+
 while True:
     x = int(input("Введите год: "))
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('cls||clear')
     if x % 4 == 0 and x % 100 != 0 or x % 400 == 0:
         print(x, "год - високосный")
     else:
