929bc9e590bf0b09dbba8970ba64ffa35474bcf2
Sergey74ua <Sergey74ua@gmail.com>
2020-08-08 15:26:28 +0300

    Update Task_03 - КНБ.py

--- Python/Task_03 - КНБ.py
+++ Python/Task_03 - КНБ.py
@@ -2,31 +2,35 @@
 
 import os, random
 
-os.system("mode con cols=60 lines=16")
+os.system("title КНБ")
+os.system("mode con cols=50 lines=10")
+os.system("color 0a")
 
 step = win = lose = draw = 0
+user = comp = 3
+name = ["Камень", "Ножницы", "Бумага", "......"]
 game = "             Начнем игру"
-user = comp = 0
 
 while True:
-    print("Всего:", step, " Побед:", win, " Поражений:", lose, " Ничьих:", draw)
     print("")
-    comp = random.randint(0, 2)
-    step += 1
-    print("     Игрок: ", user, "     Компьютер: ", comp)
+    print(" Всего:", step, " Побед:", win, " Поражений:", lose, " Ничьих:", draw)
+    print("")
+    print("    Игрок: ", name[user], "     Компьютер: ", name[comp])
     print("")
     print(game)
     print("")
-    user = int(input("Камень - 0, ножницы - 1, бумага - 2:  "))
+    user = int(input(" Камень - 0, ножницы - 1, бумага - 2:   "))
+    comp = random.randint(0, 2)
+    step += 1
     if user == comp:
-        game = "             == НИЧЬЯ =="
+        game = "              == НИЧЬЯ =="
         draw += 1
     elif (user == 0 and comp == 1) or (user == 1 and comp == 2) or (user == 2 and comp == 0):
-        game = "            !!! ПОБЕДА !!!"
+        game = "             !!! ПОБЕДА !!!"
         win += 1
     elif user >= 0 and user <= 2:
-        game = "           >> ПОРАЖЕНИЕ <<"
+        game = "            >> ПОРАЖЕНИЕ <<"
         lose += 1
     else:
-        game = "        ... неверный ввод ..."
+        game = "         ... неверный ввод ..."
     os.system('cls||clear')
