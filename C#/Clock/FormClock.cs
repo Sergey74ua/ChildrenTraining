e0290f79b94c185be53db10df93922d638f62fcd
Sergey74ua <Sergey74ua@gmail.com>
2020-08-08 23:47:39 +0300

    Create FormClock.cs

---
+++ C#/Clock/FormClock.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class FormClock : Form
+    {
+        public FormClock()
+        {
+            InitializeComponent();
+
+            SetStyle(ControlStyles.OptimizedDoubleBuffer |
+                    ControlStyles.AllPaintingInWmPaint |
+                    ControlStyles.UserPaint, true);
+            UpdateStyles();
+        }
+
+        private void timerSec_Tick(object sender, EventArgs e)
+        {
+            labelClock.Text = DateTime.Now.ToString("T");
+        }
+
+        private void FormClock_DoubleClick(object sender, EventArgs e)
+        {
+            if (FormBorderStyle == FormBorderStyle.Sizable)
+                FormBorderStyle = FormBorderStyle.None;
+            else
+                FormBorderStyle = FormBorderStyle.Sizable;
+        }
+    }
+}
