c7567bdf87dc36736bd76e98d63499f1697eb57c
Sergey74ua <Sergey74ua@gmail.com>
2020-08-25 19:19:06 +0300

    Create Olimpiada5.pas

---
+++ Pascal/Олимпиада-2019 Максим/Olimpiada5.pas
@@ -0,0 +1,14 @@
+﻿Program Olimpiada5;
+var N, s :integer;
+    arr :array[1..20] of integer;
+begin
+  Write ('Введите число данных до 20: ');
+  Readln (N);
+  Writeln ('Введите ', N, ' данных');
+  for var i := 1 to N do
+    begin
+    Readln (arr[i]);
+    if arr[i] > i then s := s + arr[i];
+    end;
+  Writeln ('Сумма особых элементов: ', s); 
+end.
