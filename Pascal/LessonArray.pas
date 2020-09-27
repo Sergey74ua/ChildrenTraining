4766a69eeb7f2f1cbab1b762f2be7ef2be37a265
Sergey74ua <Sergey74ua@gmail.com>
2020-09-27 19:48:07 +0300

    Create LessonArray.pas

---
+++ Pascal/LessonArray.pas
@@ -0,0 +1,24 @@
+﻿Program Urok;
+
+var StatArr : array [0..5] of integer;
+    DynArr  : array of integer;
+
+Begin
+  SetLength(DynArr, 10);
+  
+  StatArr[0] := 111;
+  write(StatArr[0]);
+  writeln();
+  DynArr[3] := 222;
+  
+  for var i := 0 to High(StatArr) do
+    StatArr[i] := i*i;
+  
+  write(StatArr);
+  writeln();
+  
+  for var i := 0 to DynArr.Length-1 do
+    DynArr[i] := i*i;
+  
+  write(DynArr);
+end.
