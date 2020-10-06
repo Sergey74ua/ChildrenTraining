4b8cc9dabe7ee4c572f491afa8c831f5be3e62ff
Sergey74ua <Sergey74ua@gmail.com>
2020-10-07 00:49:52 +0300

    Create LessonArray.pas

---
+++ Pascal/Уроки/LessonArray.pas
@@ -0,0 +1,46 @@
+﻿Program Urok;
+
+var StatArr : array [0..5] of integer;
+    DynArr, DynArr2 : array of integer;
+
+Begin
+  SetLength(DynArr, 10);
+  
+  StatArr[0] := 111;
+  write(StatArr[0]);
+  writeln();
+  DynArr[3] := 222;
+  
+  for var i := 0 to High(StatArr) do
+    StatArr[i] := i*i;
+  
+  write(StatArr);
+  writeln();
+  
+  for var i := 0 to Length(DynArr)-1 do
+    DynArr[i] := i*i;
+  
+  write(DynArr);
+  writeln();
+  
+  DynArr2 := Copy(DynArr);
+  DynArr2[0] := 555;
+  writeln(DynArr2);
+  
+  writeln(5.525);
+  writeln(trunc(5.525));
+  
+  writeln(5.525);
+  writeln(frac(5.525)); //дробная часть
+  
+  writeln(5.525);
+  writeln(5.525:0:2); //срез
+  writeln(round(5.525));
+  
+  writeln(5);
+  writeln(intToStr(5));
+  writeln(floatToStr(5.525));
+  
+  writeln(strToInt('5'));
+  writeln(strToFloat('5'));
+end.
