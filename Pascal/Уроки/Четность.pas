6a8c8db1273b3ecebabc9ee59a2e988e096fc9aa
Sergey74ua <Sergey74ua@gmail.com>
2020-10-07 00:50:17 +0300

    Create Четность.pas

---
+++ Pascal/Уроки/Четность.pas
@@ -0,0 +1,14 @@
+﻿Program Chetnost;
+var a, b : integer;
+  s : string;
+label l;
+Begin
+  l: s := '';
+  write('Ведите число ');
+  readln(a);
+  b := a mod 2;
+  if b = 1 then s := 'не';
+  writeln('Число ', a, ' - ', s, 'четное');
+  writeln;
+  goto l;
+end.
