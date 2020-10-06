dd443a67133395660be1012cdcbeeb5eed2eafe9
Sergey74ua <Sergey74ua@gmail.com>
2020-10-06 20:38:11 +0300

    Update Olimp_15-4.pas

--- Pascal/Олимп ejudge.cfuv.ru/Olimp_15-4.pas
+++ Pascal/Олимп ejudge.cfuv.ru/Olimp_15-4.pas
@@ -1,30 +1,43 @@
 ﻿program p;
 
-var N, i, j : integer;
+var N, S, i, j : integer;
   arr1 : array of array of char;
   arr2 : array of array of char;
 
 begin
   readln(N);
-  setLength(arr1, N);
-  for i := 0 to N-1 do
-    setLength(arr[i], N);
+  setLength(arr1, N+2);
+  for i := 0 to N do
+    setLength(arr1[i], N+2);
   setLength(arr2, N);
   for i := 0 to N-1 do
     setLength(arr2[i], N);
 
-  for i := 0 to N-1 do
+  for i := 1 to N do
   begin
-    for j := 0 to N-1 do
-      read(arr[i, j]);
+    for j := 1 to N do
+      read(arr1[i, j]);
     readln();
   end;
   
-  for i := 0 to N-1 do
-    for j := 0 to N-1 do
-      if (arr1[i, j] = 'o') then
-        
-  //решение
+  for i := 1 to N do
+    for j := 1 to N do
+    begin
+      S := 0;
+      if arr1[i - 1, j - 1] = 'o' then S := S + 1;
+      if arr1[i - 1, j] = 'o' then S := S + 1;
+      if arr1[i - 1, j + 1] = 'o' then S := S + 1;
+      if arr1[i, j - 1] = 'o' then S := S + 1;
+      if arr1[i, j + 1] = 'o' then S := S + 1;
+      if arr1[i + 1, j - 1] = 'o' then S := S + 1;
+      if arr1[i + 1, j] = 'o' then S := S + 1;
+      if arr1[i + 1, j + 1] = 'o' then S := S + 1;
+      
+      if (arr1[i, j] = '.') and (S = 3) then
+        arr2[i-1, j-1] := 'o'
+      else if (arr1[i, j] = 'o') and (S < 2) and (S > 3) then
+        arr2[i-1, j-1] := '.';
+    end;
   
   for i := 0 to N-1 do
   begin
