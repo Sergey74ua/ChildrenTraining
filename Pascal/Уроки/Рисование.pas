cea328c0b38379eb04caca6c412fe2e119dc6e69
Sergey74ua <Sergey74ua@gmail.com>
2020-10-07 00:50:11 +0300

    Create Рисование.pas

---
+++ Pascal/Уроки/Рисование.pas
@@ -0,0 +1,14 @@
+﻿Program Risovanie;
+uses GraphABC;
+
+Begin
+  Pen.Color := RGB(0, 255, 0);
+  Line(300, 100, 200, 200); 
+  SetPenColor(clBlue);
+  SetBrushColor(clRed);
+  Circle(100, 100, 50);
+  SetPenColor(clRed);
+  SetBrushColor(clYellow);
+  Rectangle(200, 250, 400, 400);
+  
+end.
