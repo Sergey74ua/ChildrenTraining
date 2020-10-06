a367ff6d33b7da3b77cb54110c5a3eea8307fcc8
Sergey74ua <Sergey74ua@gmail.com>
2020-10-06 19:54:24 +0300

    Create Olimp_15-4.pas

---
+++ Pascal/Олимп ejudge.cfuv.ru/Olimp_15-4.pas
@@ -0,0 +1,35 @@
+﻿program p;
+
+var N, i, j : integer;
+  arr1 : array of array of char;
+  arr2 : array of array of char;
+
+begin
+  readln(N);
+  setLength(arr1, N);
+  for i := 0 to N-1 do
+    setLength(arr[i], N);
+  setLength(arr2, N);
+  for i := 0 to N-1 do
+    setLength(arr2[i], N);
+
+  for i := 0 to N-1 do
+  begin
+    for j := 0 to N-1 do
+      read(arr[i, j]);
+    readln();
+  end;
+  
+  for i := 0 to N-1 do
+    for j := 0 to N-1 do
+      if (arr1[i, j] = 'o') then
+        
+  //решение
+  
+  for i := 0 to N-1 do
+  begin
+    for j := 0 to N-1 do
+      write(arr2[i, j]);
+    writeln();
+  end;
+end.
