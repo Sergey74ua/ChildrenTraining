dec8237887fa7567f72a8df249b2282dbc273202
Sergey74ua <Sergey74ua@gmail.com>
2020-10-08 22:40:27 +0300

    Create VM_01.pas

---
+++ Pascal/Уроки/VM_01.pas
@@ -0,0 +1,17 @@
+﻿program VM_01;
+var V, M, D : integer;
+
+begin
+  V := 1000;
+  M := 1;
+  D := 0;
+  
+  while V > 0 do
+  begin
+    V := V - M;
+    M := M * 2;
+    D := D + 1;
+  end;
+  
+  write(D); // 10
+end.
