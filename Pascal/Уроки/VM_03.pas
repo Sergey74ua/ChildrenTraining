169d82d5db13641c9ed8aebb1d01faf9cca030dc
Sergey74ua <Sergey74ua@gmail.com>
2020-10-08 22:44:15 +0300

    Create VM_03.pas

---
+++ Pascal/Уроки/VM_03.pas
@@ -0,0 +1,15 @@
+﻿program VM_03;
+var X, N : int64;
+
+begin
+  read(X);
+  N := 0;
+  
+  while X > 0 do
+  begin
+    X := X div 10;
+    N := N + 1;
+  end;
+  
+  write(N); //123456789 = 9
+end.
