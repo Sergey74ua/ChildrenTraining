740850fe08d5edecbddbc1e640a96aa369235452
Sergey74ua <Sergey74ua@gmail.com>
2020-10-12 21:19:55 +0300

    Create 04 - Обмен значениями.cpp

---
+++ C++/Решение задач C++ ВУЗ 2020/04 - Обмен значениями/04 - Обмен значениями.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    int A, B, temp;
+    cout << "Введите первое число (А): ";
+    cin >> A;
+    cout << "Введите второе число (B): ";
+    cin >> B;
+
+    temp = A;
+    A = B;
+    B = temp;
+    cout << "Меняем числа с дополнительной переменной (A, B): " << A << " " << B << endl;
+
+    A ^= B ^= A ^= B;
+    cout << "Меняем числа без дополнительной переменной (A, B): " << A << " " << B << endl;
+
+    system("pause>nul");
+    return 0;
+}
