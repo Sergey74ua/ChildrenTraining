e63cda716c8f505e865a45574d69295bafd04041
Sergey74ua <Sergey74ua@gmail.com>
2020-10-12 21:20:43 +0300

    Create 11 - Возведение в степень.cpp

---
+++ C++/Решение задач C++ ВУЗ 2020/11 - Возведение в степень/11 - Возведение в степень.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+
+    int x, n, s;
+
+    cout << "Введите число: ";
+    cin >> x;
+    cout << "Введите степень: ";
+    cin >> n;
+
+    s = 0;
+    if (n != 0) {
+        s = x;
+        for (int i = 1; i < n; i++)
+            s *= x;
+    }
+    cout << "Результат: " << s;
+
+    system("pause>nul");
+    return 0;
+}
