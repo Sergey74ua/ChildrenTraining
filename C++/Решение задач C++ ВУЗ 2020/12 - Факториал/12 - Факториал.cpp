dd4aeeb5e57876f4770bcf7cfb9e3c986a9f03d3
Sergey74ua <Sergey74ua@gmail.com>
2020-10-12 21:20:49 +0300

    Create 12 - Факториал.cpp

---
+++ C++/Решение задач C++ ВУЗ 2020/12 - Факториал/12 - Факториал.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+
+    int n, f;
+    cout << "Введите число: ";
+    cin >> n;
+    f = 1;
+
+    for (int i = 2; i <= n; i++)
+        f *= i;
+
+    if (f > 1e9)
+        cout << "Некорректный вывод" << endl;
+    else
+        cout << "Факториал: " << f << endl;
+
+    system("pause>nul");
+    return 0;
+}
