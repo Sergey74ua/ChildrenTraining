b0222815ceae9dadbfee0768f83aa6bbaeea20fc
Sergey74ua <Sergey74ua@gmail.com>
2020-10-12 21:19:37 +0300

    Create 01 - Простой вывод.cpp

---
+++ C++/Решение задач C++ ВУЗ 2020/01 - Простой вывод/01 - Простой вывод.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+
+    cout << "Результат выражения 2+2*2=" << 2 + 2 * 2;
+
+    system("pause>nul");
+    return 0;
+}
