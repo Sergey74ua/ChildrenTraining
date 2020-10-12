3ce6d971a13332f17c4920a845a9e1d7cbdf8adf
Sergey74ua <Sergey74ua@gmail.com>
2020-10-12 21:21:00 +0300

    Create 14 - Степень двойки.cpp

---
+++ C++/Решение задач C++ ВУЗ 2020/14 - Степень двойки/14 - Степень двойки.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+
+    long x, s, n;
+    cout << "Введите число: ";
+    cin >> x;
+
+    if (x < 0 || x > 1e15) {
+        cout << "Некорректный ввод" << endl;
+        system("pause>nul");
+        return 0;
+    }
+        
+    s = 0;
+    if (x != 0) {
+        n = 1;
+        while (n <= x) {
+            n *= 2;
+            s += 1;
+        }
+    }
+
+    cout << "Количество степеней двойки: " << s << endl;
+
+    system("pause>nul");
+    return 0;
+}
