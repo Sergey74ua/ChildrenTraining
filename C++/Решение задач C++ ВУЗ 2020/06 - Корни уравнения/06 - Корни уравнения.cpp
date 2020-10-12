4743b8ed91c8737cf3672f042aca56b481bbc314
Sergey74ua <Sergey74ua@gmail.com>
2020-10-12 21:20:11 +0300

    Create 06 - Корни уравнения.cpp

---
+++ C++/Решение задач C++ ВУЗ 2020/06 - Корни уравнения/06 - Корни уравнения.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    
+    double a, b, c, d;
+    cout << "Введите число a: ";
+    cin >> a;
+    cout << "Введите число b: ";
+    cin >> b;
+    cout << "Введите число c: ";
+    cin >> c;
+
+    d = b * b - 4 * a * c;
+
+    if (a == 0)
+        cout << "один корень: " << -c / b << endl;
+    else if (d > 0) {
+        cout << "первый корень: " << (-b + sqrt(d)) / (2 * a) << endl;
+        cout << "второй корень: " << (-b - sqrt(d)) / (2 * a) << endl;
+    }
+    else if (d == 0)
+        cout << "один корень: " << -b / (2 * a) << endl;
+    else
+        cout << "корней нет" << endl;
+
+    system("pause>nul");
+    return 0;
+}
