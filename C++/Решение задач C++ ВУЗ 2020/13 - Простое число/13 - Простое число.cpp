fc6f3da13f69d800d4a9ca0f1a5fef73d8d3e813
Sergey74ua <Sergey74ua@gmail.com>
2020-10-12 21:20:54 +0300

    Create 13 - Простое число.cpp

---
+++ C++/Решение задач C++ ВУЗ 2020/13 - Простое число/13 - Простое число.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+
+    int n, s;
+    cout << "Введите число: ";
+    cin >> n;
+
+    if (n < 2 || n > 1e9) {
+        cout << "Некорректный ввод";
+        system("pause>nul");
+        return 0;
+    }
+
+    s = 0;
+    for (int i = 1; i <= n; i++)
+        if (n % i == 0)
+            s += 1;
+
+    if (s == 2)
+        cout << "Простое число" << endl;
+    else
+        cout << "Составное число" << endl;
+
+    system("pause>nul");
+    return 0;
+}
