1bcd489e6c699fdb38744f77067a6ac900c33bad
Sergey74ua <Sergey74ua@gmail.com>
2020-10-12 21:20:23 +0300

    Create 08 - Калькулятор.cpp

---
+++ C++/Решение задач C++ ВУЗ 2020/08 - Калькулятор/08 - Калькулятор.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+
+    double A, B;
+    char act;
+
+    cout << "Введите арифметическое действие через пробел : ";
+    cin >> A >> act >> B;
+
+    if (act == '+')
+        cout << A + B;
+    else if (act == '-')
+        cout << A - B;
+    else if (act == '*')
+        cout << A * B;
+    else if (act == '/')
+        cout << A / B;
+    else
+        cout << "неопределенное действие" << endl;
+
+    system("pause>nul");
+    return 0;
+}
