b00a1da465f60f1e98aee2014348a18dccf9b92c
Sergey74ua <Sergey74ua@gmail.com>
2020-10-12 21:20:04 +0300

    Create 05 - Падение объекта.cpp

---
+++ C++/Решение задач C++ ВУЗ 2020/05 - Падение объекта/05 - Падение объекта.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+
+    double x0, v0, t;
+    cout << "Введите через пробел значения для x0, v0, t: ";
+    cin >> x0 >> v0 >> t;
+    cout << x0 + v0 * t - (9.8 * t * t) / 2;
+
+    //Объяснение: Для С++ надо брать тип данных с плавающей точкой, для более точных расчетов.
+    system("pause>nul");
+    return 0;
+}
