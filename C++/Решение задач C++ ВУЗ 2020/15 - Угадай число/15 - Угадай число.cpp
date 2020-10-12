5778be175ab78caa6d57205c472aabe2558f9214
Sergey74ua <Sergey74ua@gmail.com>
2020-10-12 21:21:05 +0300

    Create 15 - Угадай число.cpp

---
+++ C++/Решение задач C++ ВУЗ 2020/15 - Угадай число/15 - Угадай число.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+
+    int g, n, s, x;
+
+    g = 1;
+    while (g == 1) {
+        n = rand() % 100;
+        cout << "Угадайте число до 100: ";
+
+        s = 0;
+        while (true) {
+            s++;
+            cin >> x;
+            if (x == n) {
+                cout << "Поздравляю! Вы угадали" << endl;
+                break;
+            }
+            else if (x > n)
+                cout << "Загаданное число меньше" << endl;
+            else
+                cout << "Загаданное число больше" << endl;
+
+            if (s == 5) {
+                cout << "Вы проиграли. Было загадано:" << n << endl;
+                break;
+            }
+        }
+
+        cout << "Хотите начать сначала? (1 - ДА ): ";
+        cin >> g;
+        cout << endl;
+        continue;
+    }
+    return 0;
+}
