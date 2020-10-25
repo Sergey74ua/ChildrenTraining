f2c167dbe794aeabd65e7f4556097f4887897de5
Sergey74ua <Sergey74ua@gmail.com>
2020-10-25 22:27:24 +0300

    Create Olimp_6-3.pas

---
+++ Pascal/Олимп ejudge.cfuv.ru/Olimp_6-3.pas
@@ -0,0 +1,27 @@
+﻿program p;
+
+var N, i : integer;
+  X, X1, X2, X3 : int64;
+
+begin
+  readln(N);
+  
+  if N < 4 then
+    X := 1
+  else
+    begin
+      X1 := 1;
+      X2 := 1;
+      X3 := 1;
+      
+      for i := 4 to N do
+        begin
+          X := X1 + X2 + X3;
+          X3 := X2;
+          X2 := X1;
+          X1 := X;
+        end;
+    end;
+  
+  write(X);
+end.
