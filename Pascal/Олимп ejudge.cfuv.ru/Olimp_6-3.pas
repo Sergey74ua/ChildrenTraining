89087f25007c2d24b34243fafd9d3b93233943d7
Sergey74ua <Sergey74ua@gmail.com>
2020-10-29 00:09:43 +0300

    Update Olimp_6-3.pas

--- Pascal/Олимп ejudge.cfuv.ru/Olimp_6-3.pas
+++ Pascal/Олимп ejudge.cfuv.ru/Olimp_6-3.pas
@@ -1,27 +1,22 @@
 ﻿program p;
 
-var N, i : integer;
-  X, X1, X2, X3 : int64;
+var N, X, X1, X2, X3, i : int64;
 
 begin
   readln(N);
+  X1 := 1;
+  X2 := 1;
+  X3 := 1;
   
   if N < 4 then
     X := 1
   else
+    for i := 4 to N do
     begin
-      X1 := 1;
-      X2 := 1;
-      X3 := 1;
-      
-      for i := 4 to N do
-        begin
-          X := X1 + X2 + X3;
-          X3 := X2;
-          X2 := X1;
-          X1 := X;
-        end;
+      X := X1 + X2 + X3;
+      X3 := X2;
+      X2 := X1;
+      X1 := X;
     end;
-  
   write(X);
 end.
