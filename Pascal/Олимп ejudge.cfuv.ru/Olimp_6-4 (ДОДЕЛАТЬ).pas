cde6fada46f437209bf3a5ac46f89fafee8cc9e9
Sergey74ua <Sergey74ua@gmail.com>
2020-11-02 21:02:08 +0300

    Update Olimp_6-4 (ДОДЕЛАТЬ).pas

--- Pascal/Олимп ejudge.cfuv.ru/Olimp_6-4 (ДОДЕЛАТЬ).pas
+++ Pascal/Олимп ejudge.cfuv.ru/Olimp_6-4 (ДОДЕЛАТЬ).pas
@@ -1,27 +1,47 @@
 ﻿program p;
+var N, X, f, i : int64;
 
-var N, endN, X, i, j, temp : integer;
+function IsPrime(nPrime: int64): boolean;
+var iPrime, fPrime, delta: int64;
+begin
+  if (nPrime >= 5) and ((nPrime - 1) mod 6 = 0) or ((nPrime + 1) mod 6 = 0) then
+  begin
+    iPrime := 5;
+    delta := 2;
+    fPrime := trunc(sqrt(nPrime));
+    IsPrime := false;
+    while iPrime <= fPrime do
+    begin
+      if nPrime mod iPrime = 0 then
+        exit;
+      Inc(iPrime, delta);
+      delta := delta xor 6;
+    end;
+    IsPrime := true;
+  end
+  else
+    IsPrime := (nPrime = 2) or (nPrime = 3);
+end;
 
 begin
   read(N);
   X := 0;
-  endN := trunc(sqrt(N));
+  i := 1;
   
-  for i := 2 to endN do
+  f := N div 2;
+  while i <= f do
   begin
-    temp := 0;
     if N mod i = 0 then
-    begin
-      for j := 1 to i do
-        if i mod j = 0 then
-          temp := temp + 1;
-        if temp = 2 then
-        begin
-          X := X + 1;
-          temp := 0;
-        end;        
-    end;
+      if IsPrime(i) then
+        X := X + 1;
+    i := i + 2;
   end;
+  
+  if N mod 2 = 0 then
+    X := X + 1;
+  
+  if IsPrime(N) and (N <> 2) then
+    X := X + 1;
   
   write(X);
 end.
