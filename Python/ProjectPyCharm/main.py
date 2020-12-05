8f60535e32f10a546e9e08020ac88d07477ba764
Sergey74ua <Sergey74ua@gmail.com>
2020-12-05 15:11:42 +0300

    Update main.py

--- Python/ProjectPyCharm/main.py
+++ Python/ProjectPyCharm/main.py
@@ -1,6 +1,8 @@
 # Установка библиотек:
 # pip install pygame
 # pip install pyside2
+# designer.exe в папках библиотек
+# pyside2-uic in.ui -o out.py
 
 # В gitignore добавляем папку venv
 # Сохраняем список библиотек:
@@ -10,5 +12,15 @@
 
 from modules import game
 
+import sys
+from PySide2 import QtWidgets, QtGui, QtCore
+from out import Ui_Form
+
 if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    form = Ui_Form()
+    form.setupUi(Dialog)
+    Dialog.show()
+
     game.game_cycle()
