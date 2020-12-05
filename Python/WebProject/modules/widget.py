86a028ac7fcf6229527e0308009ede808ed81502
Sergey74ua <Sergey74ua@gmail.com>
2020-12-05 22:47:41 +0300

    Update widget.py

--- Python/WebProject/modules/widget.py
+++ Python/WebProject/modules/widget.py
@@ -3,8 +3,13 @@ import eel
 def start():
     eel.init("web")
 
+    #Прием сообщения по пути: html - JavaScript - Python
     @eel.expose
-    def call_in_js():
-        pass
+    def call_in_js(msg):
+        print(msg)
 
-    eel.start("index.html", size=(400, 300))
+    #Отправка сообщения по пути: Python - JavaScript - html
+    eel.call_in_python("Hello from Python!")
+
+    eel.start("index.html", mode="chrome" ,size=(400, 300))
+    #mode=(chrome, edge, opera, electron, custom, None, False ...)
