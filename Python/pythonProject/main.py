ab48adb1088ce629993979ab74d6c06090aefe44
Sergey74ua <Sergey74ua@gmail.com>
2021-01-16 18:46:54 +0300

    up signals

--- Python/pythonProject/main.py
+++ Python/pythonProject/main.py
@@ -8,33 +8,34 @@ if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
 
     form = QtWidgets.QMainWindow()
-    Ui_Form = Ui_Form()
-    Ui_Form.setupUi(form)
+    ui_Form = Ui_Form()
+    ui_Form.setupUi(form)
     form.show()
 
-    pyautogui.PAUSE = 0.01
+    pyautogui.PAUSE = 0.5
 
-    def click_pos():
-        if True:  ######## событие клика мышки по экрану ########
-            click = 0
-            while not keyboard.is_pressed('Esc'):
-                position = pyautogui.position()
-                Ui_Form.pos_X.setText(str(position.x))
-                Ui_Form.pos_Y.setText(str(position.y))
-                pyautogui.click()
-                click += 1
-                Ui_Form.click_sum.setText(str(click))
-                print("число кликов: ", click, " в позиции: ", position.x, position.y)
+    def click_CLICK():
+        click = 0
+        while not keyboard.is_pressed('Esc'):
+            pyautogui.click()
+            click += 1
+            ui_Form.click_sum.setText(str(click))
+            print("число кликов: ", click)
 
+    def click_POS():
+        pos = pyautogui.position()
+        ui_Form.pos_X.setText(str(pos.x))
+        ui_Form.pos_Y.setText(str(pos.y))
 
     # ПРОБЛЕМА !!! Клик срабатывает несколько раз и столько же запускат функцию
     # >>> решение из меню РАТОРИ не подходит, там проверяется "and" движение мышки
-    Ui_Form.GET_CLICK.clicked.connect(click_pos)
+    ui_Form.GET_CLICK.clicked.connect(click_CLICK)
+    ui_Form.GET_POS.clicked.connect(click_POS)
 
     # Вывод названий нажатых клавиш
     def print_pressed_keys(e):
         print(e, e.event_type, e.name)
-        Ui_Form.key_name.setText(str(e.name))
+        ui_Form.key_name.setText(str(e.name))
     keyboard.hook(print_pressed_keys)
 
     sys.exit(app.exec_())
