fa1cb6ab6e34c7e0af2d84005e15e8b436db9f3f
Sergey74ua <Sergey74ua@gmail.com>
2021-03-14 18:58:07 +0300

    moving Pascal

---
+++ Pascal/Олимп ejudge.cfuv.ru/Olimp_19-3.pas
@@ -0,0 +1,32 @@
+﻿program p;
+
+var N, M, i, j, temp, row, d : integer;
+    arr : array of integer;
+
+begin
+  read(N, M);
+  SetLength(arr, N);
+  
+  for i := 0 to N-1 do
+    read(arr[i]);
+  
+  for i := 0 to N-1 do
+    for j := i+1 to N-1 do
+      if arr[i] > arr[j] then
+        begin
+          temp := arr[i];
+          arr[i] := arr[j];
+          arr[j] := temp;
+        end;
+  
+  temp := 0;
+  for i := 0 to N-2 do
+    if arr[i] = arr[i+1] then
+      temp := temp+1
+    else
+    begin
+      ****************************************
+    end;
+  
+  write(arr);
+end.
