cc6ff11e0c408b5d0a22a4e1352b0dc77296d089
Sergey74ua <Sergey74ua@gmail.com>
2021-03-16 17:14:31 +0300

    Cpp + asm (factorial)

---
+++ C++/CppPlusAsm2/Main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int count;
+	int factorial = 1;
+
+	cin >> count;
+
+	__asm
+	{
+		mov esi, 1
+		mov eax, factorial
+
+		label:					;метка для цикла
+			mul esi
+			inc esi
+		cmp esi, count
+		jng label				;переход на метку
+
+		mov factorial, eax
+	}
+	cout << "факториал: " << factorial << endl;
+	system("pause");
+
+	return 0;
+}
