02cc5bc620bb8117a854a63fbe607f858c20c697
Sergey74ua <Sergey74ua@gmail.com>
2021-06-30 18:44:47 +0300

    еще задачки

--- Python/Задачник acmp.ru/0942.py
+++ Python/Задачник acmp.ru/0942.py
@@ -1,19 +1,4 @@
-N = int(input())
-arr5 = list(map(int, input().split()))
-arr3 = arr5.copy()
-arr1 = arr5.copy()
-
-arr3.reverse()
-arr1.sort()
-
-sum = [0, 0, 0]
-
-for i in range(N):
-    sum[0] += arr1[i]
-    sum[1] += arr3[i]
-    sum[2] += arr5[i]
-
-print(sum.index(min(sum))+1)
-
-# Проще, решение всегда будет 1
-# print(1)
+arr = list(map(int, input().split()))
+arr[0] //= 2
+arr[1] //= 6 
+print(min(arr))
