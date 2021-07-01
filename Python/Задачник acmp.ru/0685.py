52f3cd86f728ae01bd1de7ec5ca1fac2b874cc4d
Sergey74ua <Sergey74ua@gmail.com>
2021-07-01 21:13:26 +0300

    исправление задачек

--- Python/Задачник acmp.ru/0685.py
+++ Python/Задачник acmp.ru/0685.py
@@ -1,2 +1,6 @@
-A, B, C = map(int, input().split())
-print((A * B + B * C + C * A) * 2, A * B * C)
+A1, A2, A3, B1, B2, B3 = map(int, input().split())
+arr1 = [A1, A2, A3]
+arr1.sort()
+arr2 = [B1, B2, B3]
+arr2.sort()
+print(arr1[0]*arr2[0] + arr1[1]*arr2[1] + arr1[2]*arr2[2])
