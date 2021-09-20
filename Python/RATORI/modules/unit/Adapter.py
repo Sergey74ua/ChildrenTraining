dfeea1380dde6f081d8d6cca31ab8d250373f15a
Sergey74ua <Sergey74ua@gmail.com>
2021-09-20 19:40:09 +0300

    relocation Ratory

--- Python/RATORI/modules/unit/Adapter.py
+++
@@ -1,9 +0,0 @@
-from modules.unit.Gangster import Gangster
-
-
-class Adapter(Gangster):
-    """ Противник через адаптер"""
-
-    def draw(self, g):
-        """ Адаптер метода """
-        return self.draw_unit(g)
