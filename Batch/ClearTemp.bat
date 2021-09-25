7fab630a5b200340c64d9f97566a837d8b490c08
Sergey74ua <Sergey74ua@gmail.com>
2021-09-25 19:55:29 +0300

    хакатон

---
+++ Batch/ClearTemp.bat
@@ -0,0 +1,6 @@
+::Программа отчистки папки Temp 03.02.2019
+@echo off
+title Clear Temp
+del /Q /F /S %TEMP%\*
+
+pause
---
+++ Python/Хакатон YOUNG CODERS PARTY/10.py
@@ -0,0 +1,7 @@
+X1, Y1, R, X2, Y2 = map(int, input())
+catetX = X1 - X2
+catetY = Y1 - Y2
+if Math.Sqrt(catetX * catetX + catetY * catetY) <= R:
+    print(1)
+else:
+    print(0)
