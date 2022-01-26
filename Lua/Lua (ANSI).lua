f3bff6ddeef45e6725e09b5bf9692d2ef0e03fe2
Sergey74ua <Sergey74ua@gmail.com>
2022-01-26 21:21:48 +0300

    Update Lua (ANSI).lua

--- Lua/Lua (ANSI).lua
+++ Lua/Lua (ANSI).lua
@@ -9,40 +9,7 @@ os.setlocale('rus_rus.1251')
 X = "Hello world"
 print(X)
 
-Y = io.read('*n')
-if Y == 1 and (Y > 0 or Y == 1000) then
-	io.write(X, X)
-elseif Y ~= 0 then
-	print('не ноль')
-else
-	print('иначе')
-end
-
 a, b, c = 5, 10, 15
-arr = {1, 2, 3, 4, 5}
-print(arr[1])
-
-arr2 = {pn = 'понедельник', vt = 'вторник', sr = 'среда'}
-print(arr2.vt .. arr2.sr)
-
-function sum(x, y)
-	local z = x + y
-	return(z)
-end
-print(sum(5, 6))
-
-repeat
-	print('цикл repeat ', Y); Y = Y - 1
-until Y < 3
-
-
-while Y > 0 do
-	print('цикл while ', Y); Y = Y - 1
-end
-
-for i = 1, 5, 2 do
-	print('цикл for ', i)
-end
 
 print(type("Hello world"))      --> string
 print(type(10.4*3))             --> number
@@ -62,31 +29,25 @@ u = [[ Double brackets
        multi-line strings.]]
 t = nil
 
-print(math.random()) -- [0 до 1]
-print(math.random(10)) -- [1..до числа];
-print(math.random(10, 20)) -- [от числа..до числа].
+Y = io.read('*n')
+if Y == 1 and (Y > 0 or Y == 1000) then
+	io.write(X, X)
+elseif Y ~= 0 then
+	print('не ноль')
+else
+	print('иначе')
+end
+
+function sum(x, y)
+	local z = x + y
+	return(z)
+end
+print(sum(5, 6))
 
 -- функциональщина
 function f(x) return math.sin(x) end
 f = function (x) return math.sin(x) end
 
--- обход ключей и данных
-for key, val in pairs(arr) do  -- Table iteration.
-  print(key, val)
-end
-
-for i = 1, #arr do  -- #v is the size of v for lists.
-  print(arr[i])  -- Indices start at 1 !! SO CRAZY!
-end
--- ???? module.lua
--- local mod = require('module')
--- по сути равносильно:
--- local mod = (function ()
---  <contents of mod.lua>
--- end)()
-
--- Запуск файла
--- dofile("data/scripts/Main.lua");
 
 -- Строки: 
 print(string.upper('aaaAAA'))
@@ -100,5 +61,47 @@ print(string.byte("ABCD",4)) --> 68
 print(string.byte("ABCD")) --> 65
 print(string.len("abc")) --> 3
 print(string.rep("abcd",2)) --> abcdabcd
+
+print(math.random()) -- [0 до 1]
+print(math.random(10)) -- [1..до числа];
+print(math.random(10, 20)) -- [от числа..до числа].
+
+arr = {1, 2, 3, 4, 5}
+print(arr[1])
+
+arr2 = {pn = 'понедельник', vt = 'вторник', sr = 'среда'}
+print(arr2.vt .. arr2.sr)
+
+repeat
+	print('цикл repeat ', Y); Y = Y - 1
+until Y < 3
+
+
+while Y > 0 do
+	print('цикл while ', Y); Y = Y - 1
+end
+
+for i = 1, 5, 2 do
+	print('цикл for ', i)
+end
+
+-- обход ключей и данных
+for key, val in pairs(arr) do  -- Table iteration.
+  print(key, val)
+end
+
+for i = 1, #arr do  -- #v is the size of v for lists.
+  print(arr[i])  -- Indices start at 1 !! SO CRAZY!
+end
+
+-- файл module.lua
+-- local mod = require('module')
+-- по сути равносильно:
+-- local mod = (function ()
+--  <contents of mod.lua>
+-- end)()
+
+-- Запуск файла
+-- dofile("data/scripts/Main.lua");
 
 Y = io.read('*n')
