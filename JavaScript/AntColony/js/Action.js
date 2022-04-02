1fcea1c818a542653e8f88b7a33eee9df736920c
Sergey74ua <Sergey74ua@gmail.com>
2022-04-02 19:52:19 +0300

    Update Action.js

--- JavaScript/AntColony/js/Action.js
+++ JavaScript/AntColony/js/Action.js
@@ -6,11 +6,18 @@ class Action {
     static wait(ant) {
         console.log('функция wait', this.x);
         this.x++;
-        if (this.x>50)
-            ant.action = () => this.find();
+        if (this.x>10) {
+            ant.action = () => this.find(ant);
+            this.x=0;
+        }
     }
 
-    static find() {
+    static find(ant) {
         console.log('функция find', this.x);
+        this.x++;
+        if (this.x>10) {
+            ant.action = () => this.wait(ant);
+            this.x=0;
+        }
     }
 }
