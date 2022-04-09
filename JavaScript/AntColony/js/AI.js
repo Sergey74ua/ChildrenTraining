6330eeb2d168151431b3686b7058ac869bac16bc
Sergey74ua <Sergey74ua@gmail.com>
2022-04-09 22:04:22 +0300

    Update AI.js

--- JavaScript/AntColony/js/AI.js
+++ JavaScript/AntColony/js/AI.js
@@ -2,14 +2,15 @@
 
 class PI {
     select(ant) {
+        if (ant.pose) //для примера
+            ant.action=() => Action.wait(ant);
+        else
+            ant.action=() => Action.find(ant);
+        
         ant.target=ant.getTarget(ant.pos);
         ant.angle=ant.getAngle(ant.pos, ant.target)
-        ant.delay=Math.round(Math.random()*60+20);
-
-        if (ant.action==Action.wait(ant)) //// ТАК НЕ РАБОТАЕТ
-            ant.action=() => Action.find(ant);
-        else
-            ant.action=() => Action.wait(ant);
+        ant.delay=Math.round(Math.random()*100+20);
+        ant.pose=!ant.pose;
     }
 }
 
