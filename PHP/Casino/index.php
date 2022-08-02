a8a950e7004aafbc38e71175f95a0432ff9741bd
Sergey74ua <Sergey74ua@gmail.com>
2022-08-03 00:35:03 +0300

    sqlite

--- PHP/Casino/index.php
+++ PHP/Casino/index.php
@@ -15,7 +15,7 @@
     //Выводим все статистику с БД
     function statistics($db) {
         $connect = new PDO("sqlite:$db");
-        $sql = "SELECT sector, stat FROM Roulette";
+        $sql = "SELECT `sector`, `stat` FROM `Roulette`";
         $result = $connect->query($sql);
         $arr = [];
         foreach($result as $row)
@@ -33,7 +33,7 @@
     if(array_key_exists('bet', $_POST)) {
         $sector=rand(0, 36);
         $connect = new PDO("sqlite:$db");
-        $sql = "UPDATE Roulette SET stat=stat+1 WHERE sector = $sector";
+        $sql = "UPDATE `Roulette` SET `stat`=`stat`+1 WHERE `sector` = $sector";
         $connect->exec($sql);
         $connect = null;
     }
@@ -42,7 +42,7 @@
     //Обнуление статистики
     if(array_key_exists('reset', $_POST)) {
         $connect = new PDO("sqlite:$db");
-        $sql = "UPDATE Roulette SET stat = 0";
+        $sql = "UPDATE `Roulette` SET `stat` = 0";
         $connect->exec($sql);
         $connect = null;
     }
