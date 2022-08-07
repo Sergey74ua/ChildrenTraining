1f0644eababf716e67efbef57a14a16e144ae5fb
Sergey74ua <Sergey74ua@gmail.com>
2022-08-07 16:59:19 +0300

    rename var

--- PHP/Casino/index.php
+++ PHP/Casino/index.php
@@ -2,29 +2,29 @@
 
 <!-- Счетчик кликов на клиенте, через JS -->
 <button type='button' onClick='JS_Click()' class='button'>
-    <output id='js_count'>JS-счетчик: 0</output>
+    <output id='js_count'>JS-Счетчик: 0</output>
 </button>
 <br/><hr/><br/>
 
 <!-- Счетчик кликов с сохранением на клиенте, через PHP -->
 <?php
-    $php_count=0;
+    $count=0;
     if(isset($_POST['php_count']))
-        $php_count=preg_replace('/[^0-9]/', '', $_POST['php_count'])+1;
+        $count=preg_replace('/[^0-9]/', '', $_POST['php_count'])+1;
 ?>
 <form method='post'>
-    <input type='submit' name='php_count' class='button' value='<?='PHP-Счетчик: '.$php_count ?>' />
+    <input type='submit' name='php_count' class='button' value='<?='PHP-Счетчик: '.$count ?>' />
 </form>
 <br/><hr/><br/>
 
 <!-- Рандом по клику на сервере, через PHP -->
 <?php
-    $php_random='?';
-    if(isset($_POST['random']))
-        $php_random=rand(0, 36);
+    $random='?';
+    if(isset($_POST['php_random']))
+        $random=rand(0, 36);
 ?>
 <form method='post'>
-    <input type='submit' name='random' class='button' value='<?='PHP-Рандом: '.$php_random ?>' />
+    <input type='submit' name='php_random' class='button' value='<?='PHP-Рандом: '.$random ?>' />
 </form>
 
 <? include_once('footer.php'); ?>
--- PHP/Casino/script.js
+++ PHP/Casino/script.js
@@ -4,5 +4,5 @@
 var js_count = 0;
 function JS_Click() {
     js_count++;
-    document.getElementById("js_count").innerHTML = 'JS счетчик: ' + js_count;
+    document.getElementById("js_count").innerHTML = 'JS-Счетчик: ' + js_count;
 };
