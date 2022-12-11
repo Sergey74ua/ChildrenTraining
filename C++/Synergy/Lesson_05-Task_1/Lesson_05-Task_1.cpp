7821ad3651371bba9e73f88c8c311348b1eccdef
Sergey74ua <Sergey74ua@gmail.com>
2022-12-11 22:26:43 +0300

    Lesson 5

---
+++ C++/Synergy/Lesson_05-Task_1/Lesson_05-Task_1.cpp
@@ -0,0 +1,42 @@
+﻿//Создать 4 указателя на различные типы данных. Вывести адреса этих указателей. Попросить пользователя ввести значения для данных указателей.
+//Вывести введённые значения с помощью операции разыменования.
+
+#include <iostream> 
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+
+    int x1;
+    short x2;
+    double x3;
+    bool x4;
+
+    int *p1 = &x1;
+    short *p2 = &x2;
+    double *p3 = &x3;
+    bool *p4 = &x4;
+
+    cout << "Вывод адресов указателей:" << endl;
+    cout << p1 << endl;
+    cout << p2 << endl;
+    cout << p3 << endl;
+    cout << p4 << endl;
+    
+    cout << "Введите число (int): ";
+    cin >> x1;
+    cout << "Введите число (short): ";
+    cin >> x2;
+    cout << "Введите число (double): ";
+    cin >> x3;
+    cout << "Введите 0 или 1 (bool): ";
+    cin >> x4;
+
+    cout << "Вывод операцией разименованием:" << endl;
+    cout << *p1 << endl;
+    cout << *p2 << endl;
+    cout << *p3 << endl;
+    cout << *p4 << endl;
+
+    return 0;
+}
---
+++ C++/Synergy/Lesson_05-Task_3/Lesson_05-Task_3.cpp
@@ -0,0 +1,22 @@
+﻿//Создать и заполнить вручную динамический массив вещественных чисел двойной точности. Размер массива и его значения вводит пользователь.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    int s;
+    cout << "Введите размер массива и его значения: " << endl;
+    cin >> s;
+    double *arr = new double[s];
+    for (int i = 0; i < s; i++)
+        cin >> arr[i];
+
+    //Вывод массива
+    for (int i = 0; i < s; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
