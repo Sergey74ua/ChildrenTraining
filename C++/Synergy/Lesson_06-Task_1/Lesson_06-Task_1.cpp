60d4c6eec6f82820e90e2d1c529b825df2bb7c51
Sergey74ua <Sergey74ua@gmail.com>
2022-12-13 23:46:46 +0300

    Lesson_6

---
+++ C++/Synergy/Lesson_06-Task_1/Lesson_06-Task_1.cpp
@@ -0,0 +1,15 @@
+﻿//Создать свой вариант простейшего ГПСЧ, вывести 100 полученных чисел.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        for (int j = 1; j <= 10; j++)
+            cout << (time(0) * j + clock() * i) % 100 << " ";
+        cout << endl;
+    }
+    return 0;
+}
