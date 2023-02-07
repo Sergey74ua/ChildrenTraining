f227a85188efa9617c4612e5570312b9e995dfc5
Sergey74ua <Sergey74ua@gmail.com>
2023-02-07 23:04:42 +0300

    Base_02

---
+++ C++/Synergy/Basic_02-Task_2/Basic_02-Task_2.cpp
@@ -0,0 +1,45 @@
+﻿//1. Наследуются ли конструкторы при наследовании?
+//Ответ: Нет. Конструкторы не наследуются. Но могут быть вызваны из базового класса.
+
+//2. Создайте класс Game в экземпляре которого должны быть поле, которое содержит название игры и жанр. Присвойте полям модификаторы
+//доступа public и private соответственно. Создайте экземпляр класса и выведите на экран значения полей из экземпляра.
+
+#include <iostream>
+using namespace std;
+
+class Game
+{
+public:
+    string name;
+
+    Game(string name_ = "unknown", string genre_ = "unknown")
+    {
+        name = name_;
+        genre = set_genre(genre_);
+    }
+
+    string set_genre(string genre_)
+    {
+        if (genre_ != "unknown")
+            return genre_;
+        else
+            return "unknown";
+    }
+
+    string get_genre() {
+        return genre;
+    }
+
+private:
+    string genre;
+};
+
+int main()
+{
+    Game game = Game("Tetris", "Puzzle");
+
+    cout << "Game name: " << game.name << endl;
+    cout << "Game genre: " << game.get_genre() << endl;
+
+    return 0;
+}
---
+++ C++/Synergy/Basic_02-Task_3/Basic_02-Task_3.cpp
@@ -0,0 +1,43 @@
+﻿//3. Создайте класс Seasons, в экземпляре которого должно быть перечисление времен года, реализованное с помощью типа enum.
+//Определите метод SeasonsChange, при вызове которого будет меняться время года.Определите метод возвращающий какое время года в данный момент.
+//Создать класс ReverseSeason, наследующий от класса Seasons, в котором переопределить метод SeasonsChange так, чтобы перечисление времен года,
+//шло в обратном порядке. Протестируйте работу классов.
+
+#include <iostream>
+using namespace std;
+
+class Seasons
+{
+public:
+    static enum; /////////////////////////////////
+
+    Seasons()
+    {
+        ;
+    }
+
+    void SeasonsChange()
+    {
+        ;
+    }
+};
+
+class ReverseSeason : public Seasons
+{
+    ReverseSeason()
+    {
+        ;
+    }
+
+    void SeasonsChange()
+    {
+        ;
+    }
+};
+
+int main()
+{
+    cout << "Hello World!\n";
+
+    return 0;
+}
