ec9578758cf594ec9e3ef850cae0930edef8be30
Sergey74ua <Sergey74ua@gmail.com>
2023-02-24 14:17:33 +0300

    base-5

---
+++ C++/Synergy/Basic_05-Task_1/Basic_05-Task_1.cpp
@@ -0,0 +1,32 @@
+﻿//1. Реализовать шаблонную функцию swap, которая меняет местами значения двух переменных любого типа.
+
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+
+void swap(T a, T b)
+{
+    T c = a;
+    a = b;
+    b = c;
+    return a, b;
+}
+
+int main()
+{
+    string a, b;
+    cout << "A = ";
+    cin >> a;
+    cout << "B = ";
+    cin >> b;
+
+    swap(a, b);
+
+    cout << endl << "swap:" << endl;
+    cout << "A = " << a << endl;
+    cout << "B = " << b << endl;
+
+    return 0;
+}
---
+++ C++/Synergy/Basic_05-Task_2/Basic_05-Task_2.cpp
@@ -0,0 +1,49 @@
+﻿//2. Реализовать шаблонный класс Info, хранящий информацию о некой переменной. Соответственно должны быть поля,
+//хранящие информацию о типе данных переменной и о размере этой переменной в памяти. Должен быть реализован метод,
+//выводящий значений этих полей, а также диапазон возможных значений соответствующего типа данных.
+
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+template <class T>
+
+class Info
+{
+public:
+    T n;
+
+    Info(T n)
+    {
+        this->n = n;
+    }
+
+    void getInfo()
+    {
+        cout << "value: " << n << endl;
+        cout << "type: " << t << endl;
+        cout << "size: " << s << " byte, " << s*8 << " bit." <<  endl;
+        cout << "range: " << numeric_limits<T>::min() << " - " << numeric_limits<T>::max() << endl << endl;
+    }
+private:
+    string t = typeid(n).name();
+    int s = sizeof(n);
+};
+
+int main()
+{
+    Info<bool> x_bool(true);
+    x_bool.getInfo();
+
+    Info<char> x_char(62);
+    x_char.getInfo();
+
+    Info<short> x(123);
+    x.getInfo();
+
+    Info<float> y(123.456);
+    y.getInfo();
+
+    return 0;
+}
