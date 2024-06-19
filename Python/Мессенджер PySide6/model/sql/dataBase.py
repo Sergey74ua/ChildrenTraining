51b4350f20e8da1e466cd4bd7328bb3d7a697a73
Sergey <Sergey74ua@gmail.com>
2024-06-19 17:39:42 +0300

    remove

--- Python/Мессенджер PySide6/model/sql/dataBase.py
+++
@@ -1,7 +0,0 @@
-__author__ = "Maxim"
-
-import sqlite3
-
-# Загатовка для сохранения чата и т.д.
-class Sqlite():
-    pass
--- Python/Мессенджер PySide6/model/user.py
+++
@@ -1,9 +0,0 @@
-__author__ = "Maxim"
-
-class User():
-    def __init__(self, usn = "", uid = "", uip = "", key = ""):
-        self.name = usn
-        self.userID = uid
-        self.userIP = uip
-        self.pubKey = key
-        self.status = False
