1baf97f9685e9119112917c48951b9278541cfff
Sergey <Sergey74ua@gmail.com>
2025-10-09 02:14:51 +0300

    GUI

--- F#/TextAnalyzer/data/MainWindow.xaml.fs
+++ F#/TextAnalyzer/data/MainWindow.xaml.fs
@@ -8,19 +8,63 @@ open functions
 
 
 let initializeWindow (window: Window) =
-    let textBox = window.FindName("textBox") :?> TextBox
-    let btnEnter = window.FindName("btnEnter") :?> Button
+    
+    // Элементы управления
+    let btnInsert = window.FindName("btnInsert") :?> Button    
+    let btnScan = window.FindName("btnScan") :?> Button
     let btnClear = window.FindName("btnClear") :?> Button
+    let btnLarge = window.FindName("btnLarge") :?> Button
+    let btnSmall = window.FindName("btnSmall") :?> Button
+    let textBox = window.FindName("textBox") :?> TextBox
+    let value1 = window.FindName("value1") :?> TextBlock
+    let value2 = window.FindName("value2") :?> TextBlock
+    let value3 = window.FindName("value3") :?> TextBlock
 
-    btnEnter.Click.Add(fun _ ->
-        textBox.Text <- "Hello world!"
+    // Изменение размера шрифта с ограничениями
+    let minFontSize = 8.0
+    let maxFontSize = 24.0
+    let mutable fontSize = 12.0
+    let updateFontSize delta =
+        let newSize = fontSize + delta
+        if newSize >= minFontSize && newSize <= maxFontSize then
+            fontSize <- newSize
+            textBox.FontSize <- fontSize
+            btnLarge.IsEnabled <- fontSize < maxFontSize
+            btnSmall.IsEnabled <- fontSize > minFontSize
+
+    // Вставка текста из буфера обмена
+    btnInsert.Click.Add(fun _ ->
+        try
+            if Clipboard.ContainsText() then
+                let clipboardText = Clipboard.GetText()
+                textBox.Text <- clipboardText
+            else
+                textBox.Text <- "Буфер обмена не содержит текста"
+        with
+        | ex -> 
+            textBox.Text <- sprintf "Ошибка при вставке из буфера обмена: %s" ex.Message
     )
 
+    // Отправка текста на анализ  **** ДОПЕРЕДЕЛАТЬ ****
+    btnScan.Click.Add(fun _ ->
+        value1.Text <- scan textBox.Text |> sprintf "%d"
+        value2.Text <- scan2 textBox.Text |> sprintf "%d"
+        value3.Text <- scan3 textBox.Text |> sprintf "%d"
+    )
+
+    // Очистка текстового окна
     btnClear.Click.Add(fun _ ->
-        let t = textBox.Text
         textBox.Clear()
-        let n = count t |> sprintf "Символов: %d"
-        textBox.Text <- t + Environment.NewLine + n
+    )
+
+    // Увеличить шрифт
+    btnLarge.Click.Add(fun _ ->
+        updateFontSize 1.0
+    )
+    
+    // Уменьшить шрифт
+    btnSmall.Click.Add(fun _ ->
+        updateFontSize -1.0
     )
 
     window
--- F#/TextAnalyzer/functions.fs
+++ F#/TextAnalyzer/functions.fs
@@ -1,6 +1,18 @@
 ﻿module functions
 
 
-// Подсчет всех символов
-let count (text: string) =
+// Подсчет количества символов
+let countSymbol (text: string) =
     text.Length
+
+// Анализ текста
+let scan (text: string) =
+    countSymbol text
+
+// ДЛЯ ПРИМЕРА
+let scan2 (text: string) =
+    countSymbol text * 2
+
+// ДЛЯ ПРИМЕРА
+let scan3 (text: string) =
+    countSymbol text * 3
